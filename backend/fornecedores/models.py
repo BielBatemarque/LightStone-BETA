5362d02f6988085c8398b842771058dcd64251c4
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-08-23 00:29:11 -0300

    Criada tabela de Fornecedores

--- backend/fornecedores/models.py
+++ backend/fornecedores/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Fornecedor(models.Model):
+    nome_empresa = models.CharField(max_length=50)
+    cnpj = models.IntegerField()
+    endereco = models.TextField(max_length=150)
