88b3c696ad11f87e6a89a615b19d41c766418754
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-08-23 00:22:13 -0300

    Criada classe e tabela de clientes

--- backend/clientes/models.py
+++ backend/clientes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Ciente(models.Model):
+    nome = models.CharField(max_length=50)
+    cpf = models.IntegerField()
+    endereco = models.TextField(max_length=150)
+    data_nascimento = models.CharField(max_length=13, null=True)
+    email = models.EmailField(max_length=150)
+
