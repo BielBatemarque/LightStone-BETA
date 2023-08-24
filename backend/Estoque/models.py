b43f4b8399468e9347fc73bbeef600ae3ac3acbb
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-08-24 00:29:06 -0300

    Criação da tabela de materiais

--- backend/Estoque/models.py
+++ backend/Estoque/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from materiais.models import Material
 # Create your models here.
+
+class Estoque(models.Model):
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    quantidade_metros = models.IntegerField()
