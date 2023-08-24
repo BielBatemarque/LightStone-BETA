0e13761869846ba7431f16a36a6fc27377226b70
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-08-24 00:32:42 -0300

    cirada classe de colaborador

--- backend/colaboradores/models.py
+++ backend/colaboradores/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Colaborador(models.Model):
+    nome = models.CharField(max_length=80)
+    nascimento = models.DateField()
+    telefone = models.CharField(max_length=15)
+    cpf = models.TextField(max_length=11)
+    email = models.CharField(max_length=100)
+    
