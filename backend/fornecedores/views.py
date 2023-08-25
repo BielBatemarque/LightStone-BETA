003ccd5c50eac59230c781c9834b43d9e748966f
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-08-25 01:54:24 -0300

    viewsets de fornecedores

--- backend/fornecedores/views.py
+++ backend/fornecedores/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import FornecedorSerializers
+from .models import Fornecedor
 # Create your views here.
+
+class FornecedorViewSets(viewsets.ModelViewSet):
+    queryset = Fornecedor.objects.all()
+    serializer_class = FornecedorSerializers
