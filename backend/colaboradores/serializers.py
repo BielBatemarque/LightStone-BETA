9101e2c73c1cc3e44560ffa69ea49076371a352a
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-08-25 01:41:10 -0300

    serializer de colaborador

---
+++ backend/colaboradores/serializers.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from .models import Colaborador
+from rest_framework import routers, serializers, viewsets
+
+class ColaboradorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Colaborador
+        fields = ['nome', 'nascimento', 'telefone', 'cpf', 'email']
+
