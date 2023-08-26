945b55ce24dcd1e9ad1bffdd41f8ceb518d99ece
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-08-26 20:22:07 -0300

    criado a view sets para Estoque

--- backend/Estoque/views.py
+++ backend/Estoque/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .serializers import EstoqueSerializer
+from rest_framework import viewsets
+from .models import Estoque
 
 # Create your views here.
+
+class EstoqueViewsSets(viewsets.ModelViewSet):
+    queryset = Estoque.objects.all()
+    serializer_class = EstoqueSerializer
