df4e365252c8921a45a3560f2b46c8a9c90ad4b2
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-08-27 00:47:25 -0300

    viewsets de Material

--- backend/materiais/views.py
+++ backend/materiais/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Material
+from .serializers import MaterialSerializers
 # Create your views here.
+
+class MaterialViewSets(viewsets.ModelViewSet):
+    queryset = Material.objects.all()
+    serializer_class = MaterialSerializers
