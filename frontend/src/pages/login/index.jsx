3bdc51811d413eac397768fc3ff0a345b99c06ec
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-08-28 23:46:17 -0300

    criando form de tela de login

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -9,7 +9,13 @@ export const LoginScreen = () => {
                 </ImageContainer>
                 <SemiContainerInputs>
                     <ColumForm>
-                    
+                        <label htmlFor="username">Usuário</label>
+                        <input type="text" name="username" placeholder="Nome de Usuário"/>
+
+                        <label htmlFor="senha">Senha:</label>
+                        <input type='password' placeholder="Senha" />
+
+                        <button type="submit">Entrar</button>
                     </ColumForm>
                 </SemiContainerInputs>
             </Container>
