b8e67c49fde16e3a1e6318eab52620c00f097d97
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-08-31 03:28:27 -0300

    achado problema para deslogar e logar

--- backend/usuarios/views.py
+++ backend/usuarios/views.py
@@ -3,7 +3,9 @@ from rest_framework import routers, serializers, viewsets
 from .serializers import UserSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -16,10 +18,20 @@ class UserLoginView(APIView):
         password  = request.data.get('password')
         
         user = authenticate(request, username=username, password=password)
+        print(user)
 
         if user is not None:
             login(request, user)
+            print(request.user.is_authenticated)
             return Response({'message': 'Sucesso no login'})
         else:
             return Response({'message': 'Falha ao Logar'})
 
+class UserLogoutView(APIView):
+    def post(self, request):
+        print(request.user.is_authenticated)
+        if request.user.is_authenticated:
+            logout(request)
+            return Response({'message': 'Logout bem-sucedido'})
+        else:
+            return Response({'message': 'Nenhum usuário logado'})
