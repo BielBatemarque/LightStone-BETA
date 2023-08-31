2f41677d989e3cf2721b6ac2560cb8ece721bf3b
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-08-31 02:00:42 -0300

    criando classe para login

--- backend/usuarios/views.py
+++ backend/usuarios/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 from .serializers import UserSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import authenticate, login
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class UserLoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        senha = request.data.get('senha')
+        
+        user = authenticate(request, username=username, password=senha)
+
+        if user is not None:
+            login(request, user)
+            return Response({'message': 'Sucesso no login'})
+        else:
+            return Response({'message': 'Falha ao Logar'})
+
