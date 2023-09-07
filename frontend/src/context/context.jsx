e79033eafd44cf2be3f416c12b80d1dcaba5fa4f
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-09-07 19:23:34 -0300

    inicio da criação do contexto global

---
+++ frontend/src/context/context.jsx
@@ -0,0 +1,27 @@
+import React, { useReducer } from "react";
+
+const initialState = {
+    token:'',
+    logado: false,
+}
+
+export const globalContext = React.createContext(initialState);
+
+export const AppContext = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, globalContext);
+
+
+    return(
+        <globalContext.Provider value={{ state, dispatch}}>{children}</globalContext.Provider>
+    );
+}
+
+export const reducer = (state, action) => {
+    switch(action.type){
+        case 'autentication':
+            return {...state, token: action.payload, logado:true};
+
+        case 'logout':
+            return {...state, token: '', logado: false}
+    }
+}
