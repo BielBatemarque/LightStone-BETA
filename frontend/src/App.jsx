b6108b3795c7b4b246aa5107aaedea2388a910a2
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-09-08 19:28:40 -0300

    token front end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Content } from './components/content';
+import { useContext } from 'react';
+import { globalContext } from './context/context'
 
 function App() {
+  const token = useContext(globalContext);
+
   return (
     <div className="App">
       <BrowserRouter>
