98fafb6dce5d3f985099c72d9ef24bd670deac53
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-09-11 21:40:33 -0300

    salvando token no contexto global da aplicação

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,4 +1,10 @@
+import { useContext } from "react";
+import { globalContext } from "../../context/context";
+
 export const HomePage = () => {
+    const {state} = useContext(globalContext);
+    console.log(state);
+    
     return(
         <div className="HomePage">
             Home
