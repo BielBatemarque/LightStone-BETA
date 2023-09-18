98acc88259d5f3273f91d3fbb9c53a31b4789e24
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-09-18 20:26:12 -0300

    colocando componentes ao roteamento da sidebar

--- frontend/src/components/content/index.jsx
+++ frontend/src/components/content/index.jsx
@@ -3,6 +3,10 @@ import { LoginScreen } from '../../pages/login';
 import { HomePage } from '../../pages/Home';
 import { ContainerMain } from './styles';
 import { ClientesPage } from '../../pages/Clientes/index';
+import { ColaboradorPages } from '../../pages/Colaboradores';
+import { EstoquesPage } from '../../pages/Estoques';
+import { FornecedoresPage } from '../../pages/Fornecedores';
+import { MateriaisPage } from '../../pages/Materiais/index';
 
 export const Content = () => {
     return(
@@ -11,6 +15,10 @@ export const Content = () => {
                 <Route path='/' element={<LoginScreen />}  exact/>
                 <Route path='/Home/' element={<HomePage />} />
                 <Route path='/Clientes/' element={<ClientesPage />}/>
+                <Route path='/Colaboradores/' element={<ColaboradorPages/>}/>
+                <Route path='/Estoque/' element={<EstoquesPage/>}/>
+                <Route path='/Fornecedores/'element={<FornecedoresPage />}/>
+                <Route path='/Materiais/' element={<MateriaisPage />}/>
             </Routes>
         </ContainerMain>
     );
