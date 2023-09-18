04a68eedae755da17dfb3440b663b926dfeae51c
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-09-18 20:19:14 -0300

    implementada funcionalidade de logout na aplicação

--- frontend/src/components/SideBar/index.jsx
+++ frontend/src/components/SideBar/index.jsx
@@ -1,7 +1,36 @@
+import { useContext } from "react";
 import { MenuItem } from "../MenuItem";
 import { SideBarDiv } from "./styles";
+import { globalContext } from "../../context/context";
+import { useNavigate } from "react-router-dom";
 
 export const SideBar = () => {
+    const { state, dispatch } = useContext(globalContext);
+    const { token } = state;
+    const navigate = useNavigate();
+    
+    console.log(state, token);
+
+    const handleLogout = async () => {
+        const request = await fetch('http://localhost:8000/logout/', {
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${state.token}`,
+            }
+        });
+
+        const response = await request.json();
+        console.log(response);
+
+        if(request.status === 200){
+            dispatch({type: 'logout'});
+            navigate('/');
+        }else if(request.status === 401){
+            window.alert('Não existe sessão ativa');
+        }
+    }
+
     return(
         <SideBarDiv>
             <MenuItem text={'Menu'} linkTo={'/Home/'}/>
@@ -10,6 +39,8 @@ export const SideBar = () => {
             <MenuItem text={'Estoque'} linkTo={'/Estoque/'}/>
             <MenuItem text={'Fornecedores'} linkTo={'/Fornecedores/'}/>
             <MenuItem text={'Materiais'} linkTo={'/Materiais/'}/>
+
+            <button type="button" onClick={handleLogout}>Sair</button>
         </SideBarDiv>
     );
 }
