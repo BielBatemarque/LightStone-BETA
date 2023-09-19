b4a4890668b60b05af452d01fba002f5ec900b7d
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-09-19 00:07:57 -0300

    att nas views

--- backend/clientes/views.py
+++ backend/clientes/views.py
@@ -3,6 +3,9 @@ from .models import Cliente
 from rest_framework import routers, serializers, viewsets
 from .serializers import ClienteSerializer
 from .models import Cliente
+from rest_framework.views import APIView, Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 # Create your views here.
 
 class ClienteViewSet(viewsets.ModelViewSet):
