ea1a4ba8e957c8f651062168349eac21b60d0ad4
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-09-20 00:17:43 -0300

    iniciando testes do componente listagem

--- frontend/src/pages/Clientes/index.jsx
+++ frontend/src/pages/Clientes/index.jsx
@@ -1,7 +1,14 @@
+import { Item } from "../../components/ItemListagem";
+import { Listing } from "../../components/Listing";
+
 export const ClientesPage = () => {
     return(
         <div className="Clientes">
-            OPAOPAOPAOPA
+           <h1>Clientes</h1>
+           <Listing>
+                <Item>Cliente 1</Item>
+                <Item>Cliente 2</Item>
+           </Listing>
         </div>
     );
 }
