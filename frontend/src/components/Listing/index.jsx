95774dd57ee47ca7f3000f4c46db7cd1af8df726
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-09-20 00:17:27 -0300

    componente de listagem

---
+++ frontend/src/components/Listing/index.jsx
@@ -0,0 +1,9 @@
+import { Listagem } from "./styles";
+
+export const Listing = ({ children }) => {
+    return(
+        <Listagem>
+            {children}
+        </Listagem>
+    );
+}
---
+++ frontend/src/components/Listing/styles.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Listagem = styled.ul`
+    background-color: red;
+`;
