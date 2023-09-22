939d4ada6a911fc9aea4766b559e6e6481a9c201
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-09-22 13:04:48 -0300

    criando views de vendas

--- backend/vendas/views.py
+++ backend/vendas/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Venda
+from rest_framework import viewsets
+from .serializers import VendasSerializers
 # Create your views here.
+class VendaViewSet(viewsets.ModelViewSet):
+    queryset = Venda.objects.all()
+    serializer_class = VendasSerializers
