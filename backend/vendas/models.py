a9a29ba5ec465fe7e9264b2b71f84340dec1466b
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-09-22 02:15:33 -0300

    model de vendas

--- backend/vendas/models.py
+++ backend/vendas/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from clientes.models import Cliente
 # Create your models here.
+
+class Venda(models.Model):
+    valor_total = models.FloatField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.DO_NOTHING)
