7650acf794cd650cc5f160badc49401fb87a9ba0
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-09-24 04:11:26 -0300

    começando a estilização das listagens

--- frontend/src/components/ItemListagem/styles.js
+++ frontend/src/components/ItemListagem/styles.js
@@ -2,5 +2,12 @@ import styled from "styled-components";
 
 export const ItemListagem = styled.li`
     color: black;
-    background-color: green;
+    background-color: gray;
+    width: 90%;
+    height: 3.6rem;
+    list-style: none;
+    margin-top: 1.4rem;
+    border-radius:10px;
+    padding: 5px;
+    box-shadow: 1px 1px 3px black;
 `;
