b25b8a380ba51459a37a58dce50de7f8af0dd7cf
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-09-25 20:54:25 -0300

    fazendo a leitura de da api de clientes

--- frontend/src/pages/Clientes/index.jsx
+++ frontend/src/pages/Clientes/index.jsx
@@ -1,13 +1,29 @@
+import { useEffect, useState } from "react";
 import { Item } from "../../components/ItemListagem";
 import { Listing } from "../../components/Listing";
 
 export const ClientesPage = () => {
+    const[clientes, setClientes] = useState([]);
+
+    useEffect(() => {
+        handleCarregaClientes();
+    }, []);
+
+    const handleCarregaClientes = async () => {
+        const request = await fetch("http://localhost:8000/clientes/");
+        const response = await request.json();
+
+        setClientes(response);
+    };
+    console.log(clientes);
+
     return(
         <div className="Clientes">
            <h1>Clientes</h1>
            <Listing>
-                <Item>Cliente 1</Item>
-                <Item>Cliente 2</Item>
+                {clientes.map(cliente => (
+                    <Item>{cliente.nome}</Item>
+                ))}
            </Listing>
         </div>
     );
