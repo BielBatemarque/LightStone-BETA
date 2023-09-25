38127d97f87ee7693ff8a03f9f171a9a93cbc4a2
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-09-25 20:35:20 -0300

    ajustando styles de itens das listagens

--- frontend/src/components/ItemListagem/styles.js
+++ frontend/src/components/ItemListagem/styles.js
@@ -2,12 +2,15 @@ import styled from "styled-components";
 
 export const ItemListagem = styled.li`
     color: black;
-    background-color: gray;
+    background-color: white;
     width: 90%;
     height: 3.6rem;
     list-style: none;
     margin-top: 1.4rem;
     border-radius:10px;
+    border: 1px solid gray;
+    border-left: 5px solid gray;
     padding: 5px;
     box-shadow: 1px 1px 3px black;
+    text-align: center;
 `;
