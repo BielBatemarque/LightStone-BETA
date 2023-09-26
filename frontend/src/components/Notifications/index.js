c9ba5e3c890266d28285d8f3c0f9205f21b3bf5d
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-09-25 21:34:57 -0300

    criadas notificações de sucesso e falha

---
+++ frontend/src/components/Notifications/index.js
@@ -0,0 +1,9 @@
+import { toast } from 'react-toastify';
+
+export const SucssesNotifications = (text) => {
+        toast.success(text);
+}
+
+export const FailNotifications = (text) => {
+    toast.error(text);
+}
