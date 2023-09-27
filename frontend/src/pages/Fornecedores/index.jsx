e064a8c53ab259e304cc7bb82b9403152add1cca
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-09-26 23:26:51 -0300

    adicionando listagem de forncedores

--- frontend/src/pages/Fornecedores/index.jsx
+++ frontend/src/pages/Fornecedores/index.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import { Button } from "../../components/Button";
+import { FlexCointainer } from "../../components/FlexContainer";
+import { Listing } from "../../components/Listing";
+import { Title } from "../../components/Title";
+import { Item } from "../../components/ItemListagem";
+
 export const FornecedoresPage = () => {
+    const [fornecedores, setFornecedores] = useState([]);
+
+    const handleLoadFornecedores = async () => {
+        const response = await fetch('http://localhost:8000/fornecedores/');
+        const request = await response.json();
+
+        setFornecedores(request);
+    }
+
+    useEffect(() => {
+        handleLoadFornecedores();
+    }, []);
+
     return(
         <div className="Fornecedores">
-            Fornecedores
+            <FlexCointainer pontas={true} size={'93%'}>
+                <Title>Fornecedores</Title>
+                <Button children={'Novo Fornecedor'}/>
+            </FlexCointainer>
+            <Listing>
+                {fornecedores.map(fornecedor => (
+                    <Item>{fornecedor.nome_empresa}</Item>
+                ))}
+            </Listing>
         </div>
     );
 }
