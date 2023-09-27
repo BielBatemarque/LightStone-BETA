b2e104a3a85b84fd9a741829830dd14bfdb842f4
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-09-27 00:24:05 -0300

    criando listagem de materiais

--- frontend/src/pages/Materiais/index.jsx
+++ frontend/src/pages/Materiais/index.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../../components/Button";
 import { FlexCointainer } from "../../components/FlexContainer";
 import { Listing } from "../../components/Listing";
 import { Title } from "../../components/Title";
+import { Item } from "../../components/ItemListagem";
 
 export const MateriaisPage = () => {
     const [materiais, setMateriais] = useState([]);
@@ -12,7 +13,13 @@ export const MateriaisPage = () => {
         const response = await request.json();
 
         setMateriais(response);
-    }
+    };
+
+    useEffect(() => {
+        handleLoadingMateriais();
+    });
+
+
     return(
         <div className="materiais">
             <FlexCointainer pontas={true} size={'93%'}>
@@ -20,7 +27,9 @@ export const MateriaisPage = () => {
                 <Button>Novo Material</Button>
             </FlexCointainer>
             <Listing>
-
+                {materiais.map(material => (
+                    <Item>{material.nome}</Item>
+                ))}
             </Listing>
         </div>
     );
