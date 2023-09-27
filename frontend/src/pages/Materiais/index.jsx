4459b748023551748a9ec6e084b83da7a8d84e59
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-09-27 00:20:17 -0300

    criando listagem de materiais

--- frontend/src/pages/Materiais/index.jsx
+++ frontend/src/pages/Materiais/index.jsx
@@ -1,7 +1,14 @@
+import { Button } from "../../components/Button";
+import { FlexCointainer } from "../../components/FlexContainer";
+import { Title } from "../../components/Title";
+
 export const MateriaisPage = () => {
     return(
         <div className="materiais">
-            Materiais
+            <FlexCointainer pontas={true} size={'93%'}>
+                <Title>Materiais</Title>
+                <Button>Novo Material</Button>
+            </FlexCointainer>
         </div>
     );
 }
