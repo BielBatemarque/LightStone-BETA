f4e9a5ed8e71314495357045454a8764a3a00a83
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-09-27 00:25:31 -0300

    adicionando ação ao btn de cadastrar material

--- frontend/src/pages/Materiais/index.jsx
+++ frontend/src/pages/Materiais/index.jsx
@@ -4,9 +4,11 @@ import { FlexCointainer } from "../../components/FlexContainer";
 import { Listing } from "../../components/Listing";
 import { Title } from "../../components/Title";
 import { Item } from "../../components/ItemListagem";
+import { useNavigate } from "react-router-dom";
 
 export const MateriaisPage = () => {
     const [materiais, setMateriais] = useState([]);
+    const navigate = useNavigate();
 
     const handleLoadingMateriais = async() => {
         const request = await fetch('http://localhost:8000/materiais/');
@@ -24,7 +26,7 @@ export const MateriaisPage = () => {
         <div className="materiais">
             <FlexCointainer pontas={true} size={'93%'}>
                 <Title>Materiais</Title>
-                <Button>Novo Material</Button>
+                <Button action={() => navigate('/Materiais/cadastrarMaterial/')}>Novo Material</Button>
             </FlexCointainer>
             <Listing>
                 {materiais.map(material => (
