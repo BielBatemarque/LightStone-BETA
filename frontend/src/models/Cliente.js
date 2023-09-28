f536fb02e4f1764510ecd3be35e6a17e0f661dd2
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-09-28 02:40:20 -0300

    Criada classe para cliente

---
+++ frontend/src/models/Cliente.js
@@ -0,0 +1,9 @@
+export class Cliente{
+    constructor(nome, cpf, endereco, data_nascimento, email){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.data_nascimento = data_nascimento;
+        this.email = email;
+    }
+}
