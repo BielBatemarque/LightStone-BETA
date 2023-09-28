864df77c169c41bad6ca3c133d4d952f29395b26
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-09-28 00:18:49 -0300

    adicionando a propiedade key

--- frontend/src/pages/Vendas/index.jsx
+++ frontend/src/pages/Vendas/index.jsx
@@ -1,14 +1,39 @@
+import { useEffect, useState } from "react";
 import { FlexCointainer } from "../../components/FlexContainer";
 import { Listing } from "../../components/Listing";
 import { Title } from "../../components/Title";
+import { Item } from '../../components/ItemListagem';
+import { Button } from '../../components/Button';
+import { useNavigate } from 'react-router-dom';
 
 export const VendasPage = () => {
+    const [vendas, setVendas] = useState([]);
+    const navigate = useNavigate();
+
+    const handleLoadVendas = async () => {
+        const request = await fetch('http://localhost:8000/vendas/');
+        const response = await request.json();
+
+        setVendas(response);
+    }
+
+    useEffect(() => {
+        handleLoadVendas();
+    }, []);
+
+    console.log(vendas)
+
     return(
         <div className="VendasPage">
-            <FlexCointainer>
+            <FlexCointainer pontas={true} size={'93%'}>
                 <Title>Vendas</Title>
+                <Button action={() => navigate('//')}>Cadastrar Venda</Button>
             </FlexCointainer>
-            <Listing></Listing>
+            <Listing>
+                {vendas.map((venda, index) => (
+                    <Item key={index}>{venda.valor_total}</Item>
+                ))}
+            </Listing>
         </div>
     );
 }
