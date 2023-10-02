c8f7019db8e0bc6ebdf12bbba2ee7035a8e993fa
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-10-02 19:21:32 -0300

    classe de colaborador

---
+++ frontend/src/models/Colaborador.js
@@ -0,0 +1,10 @@
+export class Colaborador{
+    constructor(nome, nascimento, telefone, cpf, email, cargo){
+        this.nome = nome;
+        this.nascimento = nascimento;
+        this.telefone = telefone;
+        this.cpf = cpf;
+        this.email = email;
+        this.cargo = cargo;
+    }
+}
