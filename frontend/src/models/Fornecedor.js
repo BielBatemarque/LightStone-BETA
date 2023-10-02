5b27de724853a0b7d7f27ac1766697263d251878
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-10-02 19:23:21 -0300

    classe de forncedor

---
+++ frontend/src/models/Fornecedor.js
@@ -0,0 +1,7 @@
+export class Fornecedor{
+    constructor(nomeEmpresa, cnpj, endereco){
+        this.nome_empresa = nomeEmpresa;
+        this.cnpj = cnpj;
+        this.endereco = endereco;
+    }
+}
