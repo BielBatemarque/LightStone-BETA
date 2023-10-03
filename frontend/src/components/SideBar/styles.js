1149b35a49d613a25b144fe7991367902abea039
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-10-03 10:10:35 -0300

    criado componente de card na sidebar

--- frontend/src/components/SideBar/styles.js
+++ frontend/src/components/SideBar/styles.js
@@ -14,8 +14,22 @@ export const StyledCOllum = styled.div`
 
 export const LogoutButton = styled.button`
     order: 1;
-    margin-top: auto;
     margin-bottom:15px;
     width: 70%;
     align-self: center;
+`;
+
+export const HelloCard = styled.div`
+    order: 1;
+    background-color: rgb(20, 20, 20);
+    width:70%;
+    margin-top: auto;
+    margin-bottom: 1rem;
+    align-self: center;
+    border-radius: 5px; 
+    color: rgb(159, 158, 158);
+    text-align: center;
+    padding: 1px;
+    font-size: 1.1rem;
+
 `;
