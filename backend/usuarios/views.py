a36b9aa42f4c6072f9b7be04338a600e97a4f4bc
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-10-03 10:28:07 -0300

    adicionando username ao retorno da api

--- backend/usuarios/views.py
+++ backend/usuarios/views.py
@@ -28,7 +28,8 @@ class UserLoginView(APIView):
             if user is not None:
                 login(request, user)
                 token, created = Token.objects.get_or_create(user=user)
-                return Response({'token': token.key}, status=status.HTTP_200_OK)
+                print(username)
+                return Response({'token': token.key, 'user': username}, status=status.HTTP_200_OK)
             else:
                 return Response({'error': 'Credenciais inválidas.'}, status=status.HTTP_401_UNAUTHORIZED)
         except Exception as e:
