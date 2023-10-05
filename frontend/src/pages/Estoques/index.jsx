da1ccd0b599a4414ddd466e6a8f8093b4aa71316
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-10-05 02:27:14 -0300

    criando listagem de estoques

--- frontend/src/pages/Estoques/index.jsx
+++ frontend/src/pages/Estoques/index.jsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react';
 import { Button } from '../../components/Button';
 import { FlexCointainer } from '../../components/FlexContainer';
 import { Listing } from '../../components/Listing';
 import { Title } from '../../components/Title/index';
+import { Item } from '../../components/ItemListagem';
 
 export const EstoquesPage = () => {
-    
+    const [estoques, setEstoques] = useState([]);
+
+    const handleLoadingEstoques = async () => {
+        const request = await fetch('http://localhost:8000/estoques/');
+        const response = await request.json()
+
+        setEstoques(response);
+    }
+
+    useEffect(() => {
+        handleLoadingEstoques();
+    }, []);
+
     return(
         <>
            <FlexCointainer pontas='true' size={'93%'}>
@@ -12,7 +26,9 @@ export const EstoquesPage = () => {
                 <Button >Registrar Entrada</Button>
            </FlexCointainer>
            <Listing>
-
+                {estoques.map((estoque, index) => (
+                    <Item key={index} >{estoque.material}</Item>
+                ))}
            </Listing>
         </>  
     );
