5c7d3db350ef63eb44db0dc22fbba540087e6589
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-10-09 17:44:52 -0300

    estrutura do form de cadastro de forncedor

--- frontend/src/pages/Fornecedores/cadastrarFornecedor.jsx
+++ frontend/src/pages/Fornecedores/cadastrarFornecedor.jsx
@@ -4,6 +4,11 @@ export const CadastrarFornecedor = () => {
     return(
         <>
            <Title>Cadastrar Fornecedor</Title>
+           <form action="">
+            <input type="text" placeholder="Nome da empresa" /> <br />
+            <input type="text" placeholder="CNPJ" /><br />
+            <input type="text" placeholder="Endereço" /> <br />
+           </form>
         </>
     );
 }
