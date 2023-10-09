0eb624af1f0f22ae85cc8a9b897ee3a7a4d936a6
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-10-09 17:47:07 -0300

    estrutura form de material

--- frontend/src/pages/Materiais/cadastrarMaterial.jsx
+++ frontend/src/pages/Materiais/cadastrarMaterial.jsx
@@ -4,6 +4,13 @@ export const CadastrarMaterialPage = () => {
     return(
         <>
             <Title>Cadastrar Material</Title>
+            <form action="">
+                <input type="text" placeholder="Nome do material"/> <br />
+                <input type="text" placeholder="Cor base"/> <br />
+                <select name="" id="">
+                    <option value="">Selecione</option>
+                </select>
+            </form>
         </>
     );
 }
