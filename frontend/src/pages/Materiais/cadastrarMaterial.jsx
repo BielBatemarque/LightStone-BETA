48ca5a83c6bd3c1dc91ea4124e44bd1cb33f1303
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-10-16 20:35:11 -0300

    onchange de cadastrar material

--- frontend/src/pages/Materiais/cadastrarMaterial.jsx
+++ frontend/src/pages/Materiais/cadastrarMaterial.jsx
@@ -4,21 +4,34 @@ import { Material } from '../../models/Material';
 import { Title } from "../../components/Title";
 
 export const CadastrarMaterialPage = () => {
-    const [material, setMaterial] = useState(new Material())
+    const [material, setMaterial] = useState(new Material());
+    const [fornecedores, setFornecedores] = useState([]);
+
+    // const handleLoadFornecedores = async () => {
+    //     const request = await fetch('')
+    // };
+
+
     const handleCadastrarMaterial = async (e) => {
         e.preventDefault();
         window.alert('Cadastrar Material');
     };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setMaterial({...material, [name]: value});
+    };
+
+    console.log(material);
     
     return(
         <>
             <Title>Cadastrar Material</Title>
             <form action="">
-                <input type="text" placeholder="Nome do material"/> <br />
-                <input type="text" placeholder="Cor base"/> <br />
-                <select name="" id="">
+                <input type="text" placeholder="Nome do material" name="nome" onChange={handleChange}/> <br />
+                <input type="text" placeholder="Cor base" name="cor_base" onChange={handleChange}/> <br />
+                <select name="" id="" onChange={handleChange}>
                     <option value="">Fornecedor</option>
-
                 </select>
                 <Button action={handleCadastrarMaterial}>Cadastrar Material</Button>
             </form>
