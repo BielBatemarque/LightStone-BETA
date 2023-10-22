9f75220f37e5005eff9b6d6b239a85a103d6f652
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-10-22 00:12:18 -0300

    criado o componente de maisInformacoesColaborador

--- frontend/src/pages/Colaboradores/MaisInformacoesColaborador.jsx
+++ frontend/src/pages/Colaboradores/MaisInformacoesColaborador.jsx
@@ -1,11 +1,25 @@
 import { useParams } from "react-router-dom";
 import { Title } from "../../components/Title";
+import { useEffect, useState } from "react";
 
 export const MaisInformacoesColaborador = () => {
+    const [colaborador, setColaborador] = useState({});
     const { id } = useParams(':id');
+
+    const handleLoadColaborador = async () => {
+        const request = await fetch(`http://localhost:8000/colaboradores/${id}`);
+        const response = await request.json();
+
+        setColaborador(response);
+    };
+
+    useEffect(() => {
+        handleLoadColaborador();
+    }, []);
+
     return(
         <>
-            <Title>Colaborador: {id}</Title>
+            <Title>Colaborador: {colaborador.nome}</Title>
         </>
     );
 };
