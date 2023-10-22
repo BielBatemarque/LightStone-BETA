4fda064672e2ea8b1c268137c983570afa540bdd
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-10-21 21:16:45 -0300

    levando informações de forncedor

--- frontend/src/pages/Fornecedores/MaisInformacoesFornecedor.jsx
+++ frontend/src/pages/Fornecedores/MaisInformacoesFornecedor.jsx
@@ -1,7 +1,26 @@
-export const MaisInformacoesFornecedor = ({ fornecedor }) => {
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+export const MaisInformacoesFornecedor = () => {
+    const [fornecedor, setFornecedor] = useState({});
+    const { id } = useParams(':id');
+
+    const handleLoadFornecedor = async () => {
+        const request = await fetch(`http://localhost:8000/fornecedores/${id}`);
+        const response = await request.json();
+
+        setFornecedor(response);
+    };
+
+    console.log(id)
+
+    useEffect(() => {
+        handleLoadFornecedor();
+    }, [id]);
+
     return(
         <>
-            <h1>Opa</h1>
+            <h1>Fornecedor: {fornecedor.nome_empresa}</h1>
         </>
     );
 }
