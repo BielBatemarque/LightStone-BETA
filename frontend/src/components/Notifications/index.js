b0f8bf799b9a42f27d317247245aad71e057402c
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-10-30 22:49:00 -0300

    arrumando arquivos js

--- frontend/src/components/Notifications/index.js
+++ frontend/src/components/Notifications/index.js
@@ -2,12 +2,12 @@ import { toast } from 'react-toastify';
 
 export const SucssesNotifications = (text) => {
         toast.success(text);
-}
+};
 
 export const FailNotifications = (text) => {
     toast.error(text);
-}
+};
 
 export const AtentionNotification = (text) => {
     toast.warn(text)
-}
+};
