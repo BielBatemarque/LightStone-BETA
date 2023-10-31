ad853fee25d482cf65dade0cbf2f843fdc204a14
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-10-30 22:49:19 -0300

    arrumando hook

--- frontend/src/hooks/useLocalStorage.js
+++ frontend/src/hooks/useLocalStorage.js
@@ -1,8 +1,6 @@
 export const useLocalStorage = (key, initialValue) => {
-      // Obtém o valor do localStorage pelo key fornecido ou o valor inicial
   const storedValue = localStorage.getItem(key) ? JSON.parse(localStorage.getItem(key)) : initialValue;
 
-  // Cria um estado para armazenar o valor atual
   const [value, setValue] = useState(storedValue);
 
   const setStoredValue = (newValue) => {
