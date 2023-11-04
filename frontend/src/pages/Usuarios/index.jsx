b594d482b86284e46070b4ee0d4ee28cc41babed
BielBatemarque <gabrielb.sdeolveira12@gmail.com>
2023-11-04 13:29:48 -0300

    criado listagem de usuários

--- frontend/src/pages/Usuarios/index.jsx
+++ frontend/src/pages/Usuarios/index.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react';
 import { Button } from '../../components/Button';
 import { FlexCointainer } from '../../components/FlexContainer';
 import { Listing } from '../../components/Listing';
 import { Title } from '../../components/Title/index';
+import { Item } from '../../components/ItemListagem';
 
 export const Usuarios = () => {
+    const [users, setUsers] = useState([]);
+
+    const handleLoadUsers = async () => {
+        try{
+            const request = await fetch('http://localhost:8000/users/');
+            const response = await request.json();
+            setUsers(response);
+
+        }catch(e){
+            console.log(`Erro na requisição a API: ${String(e)}`);
+        }
+        
+    };
+
+    useEffect(() => {
+        handleLoadUsers();
+    }, []);
+
     return(
         <>
             <FlexCointainer size={'93%'} pontas='true'>
@@ -11,7 +31,9 @@ export const Usuarios = () => {
                 <Button>Cadastrar usuário</Button>
             </FlexCointainer>
             <Listing>
-                {}
+                {users.map((user, index) => (
+                    <Item key={index}>{user.username}</Item>
+                ))}
             </Listing>
         </>
     );
