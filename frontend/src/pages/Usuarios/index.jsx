1246146f62c3015f5cb1a42711ba8b0ee24d5d4a
BielBatemarque <gabrielb.sdeolveira12@gmail.com>
2023-11-04 13:21:05 -0300

    criando pagina de usuários

--- frontend/src/pages/Usuarios/index.jsx
+++ frontend/src/pages/Usuarios/index.jsx
@@ -1,9 +1,18 @@
+import { Button } from '../../components/Button';
+import { FlexCointainer } from '../../components/FlexContainer';
+import { Listing } from '../../components/Listing';
 import { Title } from '../../components/Title/index';
 
 export const Usuarios = () => {
     return(
         <>
-            <Title>Usuários</Title>
+            <FlexCointainer size={'93%'} pontas='true'>
+                <Title>Usuários</Title>
+                <Button>Cadastrar usuário</Button>
+            </FlexCointainer>
+            <Listing>
+                {}
+            </Listing>
         </>
     );
 };
