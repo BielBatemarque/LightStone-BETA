93b5e10727f8e7e88158a21b7b66f4fa45e46639
BielBatemarque <gabrielb.sdeolveira12@gmail.com>
2023-11-05 23:48:57 -0300

    form de update forncedor

--- frontend/src/pages/Fornecedores/MaisInformacoesFornecedor.jsx
+++ frontend/src/pages/Fornecedores/MaisInformacoesFornecedor.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Button } from '../../components/Button';
 
 export const MaisInformacoesFornecedor = () => {
     const [fornecedor, setFornecedor] = useState({});
@@ -18,9 +19,22 @@ export const MaisInformacoesFornecedor = () => {
         handleLoadFornecedor();
     }, [id]);
 
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setFornecedor({...fornecedor, [name]: value});
+    };
+
     return(
         <>
             <h1>Fornecedor: {fornecedor.nome_empresa}</h1>
+
+            <form>
+                <input type="text" placeholder="Nome da empresa" name="nome_empresa" onChange={handleChange} value={fornecedor.nome_empresa}/> <br />
+                <input type="text" placeholder="CNPJ" name="cnpj" onChange={handleChange} value={fornecedor.cnpj}/><br />
+                <input type="text" placeholder="Endereço" name="endereco" onChange={handleChange} value={fornecedor.endereco}/> <br />
+                <Button>Cadastrar Fornecedor</Button>
+            </form>
         </>
     );
 }
