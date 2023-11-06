e74cb65d578fbe5ef27aa5c78a8a4f89f62b457a
BielBatemarque <gabrielb.sdeolveira12@gmail.com>
2023-11-06 00:03:58 -0300

    update de forncedor

--- frontend/src/pages/Fornecedores/MaisInformacoesFornecedor.jsx
+++ frontend/src/pages/Fornecedores/MaisInformacoesFornecedor.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Button } from '../../components/Button';
+import { useAuth } from "../../hooks/useAuth";
+import { FailNotifications, SucssesNotifications } from "../../components/Notifications";
 
 export const MaisInformacoesFornecedor = () => {
     const [fornecedor, setFornecedor] = useState({});
     const { id } = useParams(':id');
+    const navigate = useNavigate();
+    const { state } = useAuth();
 
     const handleLoadFornecedor = async () => {
         const request = await fetch(`http://localhost:8000/fornecedores/${id}`);
@@ -25,16 +29,41 @@ export const MaisInformacoesFornecedor = () => {
         setFornecedor({...fornecedor, [name]: value});
     };
 
+    const handleUpdateFornecedor = async (e) => {
+        e.preventDefault();
+        
+        const request = await fetch(`http://localhost:8000/fornecedores/${id}/`,{
+            method: 'PUT',
+            headers:{
+                'Content-Type': 'application/json',
+                'Authorization' : `Token ${state.token}`,
+            },
+            body: JSON.stringify(fornecedor),
+        });
+
+        if (request.ok){
+            SucssesNotifications('Fornecedor Editado com Sucesso');
+            navigate('/Fornecedores/');
+        }else{
+            FailNotifications('Não foi possivel editar Fornecedor');
+        }
+
+    };
+
+    const handleDeleteFornecedor = async () => {
+        const request = await fetch(``, {})
+    };
+
     return(
         <>
             <h1>Fornecedor: {fornecedor.nome_empresa}</h1>
 
-            <form>
+            <form onSubmit={handleUpdateFornecedor}>
                 <input type="text" placeholder="Nome da empresa" name="nome_empresa" onChange={handleChange} value={fornecedor.nome_empresa}/> <br />
                 <input type="text" placeholder="CNPJ" name="cnpj" onChange={handleChange} value={fornecedor.cnpj}/><br />
                 <input type="text" placeholder="Endereço" name="endereco" onChange={handleChange} value={fornecedor.endereco}/> <br />
-                <Button>Cadastrar Fornecedor</Button>
+                <Button>Editar Fornecedor</Button>
             </form>
         </>
     );
-}
+};
