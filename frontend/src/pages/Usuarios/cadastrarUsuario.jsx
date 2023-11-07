171a78ddd53902d0c6102c7a12b2d4c090fa9c3c
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-11-06 22:48:14 -0300

    cadastro de users

--- frontend/src/pages/Usuarios/cadastrarUsuario.jsx
+++ frontend/src/pages/Usuarios/cadastrarUsuario.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 import { Button } from "../../components/Button";
 import { Title } from "../../components/Title";
+import { useAuth } from "../../hooks/useAuth";
+import { FailNotifications, SucssesNotifications } from "../../components/Notifications";
+import { useNavigate } from "react-router-dom";
 
 export const CadastrarUsuarioPage = () => {
     const [user, setUser] = useState({});
+    const { state } = useAuth();
+    const navigate = useNavigate();
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -12,11 +17,34 @@ export const CadastrarUsuarioPage = () => {
 
     console.log(user);
 
+    const handleCadastrarUser = async (e) => {
+        e.preventDefault();
+
+        const request = await fetch('http://localhost:8000/users/', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json',
+                'Authorization' : `Token ${state.token}`
+            },
+            body: JSON.stringify(user),
+        });
+
+        if (request.ok){
+            SucssesNotifications('usuário cadastrado com sucesso');
+            navigate('/Usuarios/');
+        }else{
+            FailNotifications('Não foi possivel cadastrar Usuário');
+        }
+    };
+
+
+
+
     return(
         <>
             <Title>Cadastrar Usuário</Title>
 
-            <form>
+            <form onSubmit={handleCadastrarUser}>
                 <input type="text" name="username" placeholder="nome de usuário" onChange={handleChange}/><br />
                 <input type="email" name="email" placeholder="Email" onChange={handleChange} /><br />
                 <select name="is_staff" onChange={handleChange}>
