b014966730ce778296f09ab42212e86c79ebd908
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-11-06 22:35:08 -0300

    form para cadastro de usuário

--- frontend/src/pages/Usuarios/cadastrarUsuario.jsx
+++ frontend/src/pages/Usuarios/cadastrarUsuario.jsx
@@ -1,9 +1,21 @@
+import { Button } from "../../components/Button";
 import { Title } from "../../components/Title";
 
 export const CadastrarUsuarioPage = () => {
     return(
         <>
             <Title>Cadastrar Usuário</Title>
+
+            <form>
+                <input type="text" name="username" placeholder="nome de usuário"/><br />
+                <input type="email" name="email" placeholder="Email" /><br />
+                <select name="is_staff">
+                    <option value="true">Sim</option>
+                    <option value="false">Não</option>
+                </select>   <br />
+                <input type="password" name="password" placeholder="Senha"/><br />
+                <Button>Cadastrar Usuário</Button>
+            </form>
         </>
     );
 };
