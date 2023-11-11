763b3a6099905e78e65a480b30a78aface88bcb3
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-11-10 22:22:23 -0300

    baixando pdf pelo lado do client

--- frontend/src/pages/Relatorios/index.jsx
+++ frontend/src/pages/Relatorios/index.jsx
@@ -1,10 +1,36 @@
+import { Button } from "../../components/Button";
 import { Title } from "../../components/Title";
 
 export const RelatoriosPage = () => {
+    const fetchData = async () => {
+        try {
+          const response = await fetch('http://localhost:8000/gerar_pdf/');
+          
+          if (response.ok) {
+           const blob = await response.blob();
+           const url = window.URL.createObjectURL(blob);
+
+           const a = document.createElement('a');
+           a.href = url;
+           a.download = 'relatorio.pdf';
+           document.body.appendChild(a);
+           a.click();
+ 
+           document.body.removeChild(a);
+
+          } else {
+            console.error('Erro na requisição:', response.statusText);
+          }
+        } catch (error) {
+          console.error('Erro na requisição:', error);
+        }
+      };
+
     return(
        <>
             <Title>Relatórios</Title>
             <p>Tela da qual poderão ser impressos diversos relatórios no formato PDF</p>
+            <Button action={fetchData}>Baixar PDF</Button>
        </> 
     );
 };
