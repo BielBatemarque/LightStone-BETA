9b4a5adfd774f479d7e55f8ed850d6a6f5ab7f68
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-11-25 15:58:51 -0300

    feat: requisição para carregar estoque

--- frontend/src/pages/Estoques/MaisInformacoesEstoque.jsx
+++ frontend/src/pages/Estoques/MaisInformacoesEstoque.jsx
@@ -1,9 +1,26 @@
+import { useParams } from "react-router-dom";
 import { Title } from "../../components/Title";
+import { useEffect, useState } from "react";
+import { Estoque } from '../../models/Estoque';
 
 export const MaisInformacoesEstoque = () => {
+    const [estoque, setEstoque] = useState(new Estoque());
+    const { id } = useParams(':id');
+
+    const handleLoadEstoque = async () => {
+        const request = await fetch(`http://localhost:8000/estoques/${id}/`);
+        const response = await request.json();
+
+        setEstoque(response);
+    };
+
+    useEffect(() => {
+        handleLoadEstoque();
+    });
+
     return(
         <>
-            <Title>Mais Informações </Title>
+            <Title>Mais Informações: {estoque.material}</Title>
         </>
     );
 };
