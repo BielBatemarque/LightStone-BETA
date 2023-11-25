4ec820c5955b0c023b73ab8e4f374decaba2bfd0
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-11-25 14:50:41 -0300

    feat: adicionano movimentações de estoque ao roteamento da API

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,7 +5,7 @@ from usuarios.views import UserViewSet
 from clientes.views import ClienteViewSet
 from colaboradores.views import ColaboradorViewsSets
 from fornecedores.views import FornecedorViewSets
-from Estoque.views import EstoqueViewsSets
+from Estoque.views import EstoqueViewsSets, EntradaDeEstoque, SaidaDeEstoque
 from materiais.views import MaterialViewSets
 from usuarios.views import UserLoginView, LogoutView
 from vendas.views import VendaViewSet
@@ -27,4 +27,6 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='user-login'),
     path('logout/', LogoutView.as_view(), name='user-logout'),
     path('gerar_pdf/', PDFView.as_view(), name='gerar_pdf'),
+    path('entrada_estpque', EntradaDeEstoque.as_view(), name='entrada_estoque'),
+    path('saida_estoque', SaidaDeEstoque.as_view(), name='saida_estoque'),
 ]
