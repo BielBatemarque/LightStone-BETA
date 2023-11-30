36bb51dadb0389ccbfec0eeb45ca8d47ea42767d
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-11-29 22:17:12 -0300

    feat requisição para material

--- frontend/src/pages/Estoques/MaisInformacoesEstoque.jsx
+++ frontend/src/pages/Estoques/MaisInformacoesEstoque.jsx
@@ -6,21 +6,31 @@ import { Estoque } from '../../models/Estoque';
 export const MaisInformacoesEstoque = () => {
     const [estoque, setEstoque] = useState(new Estoque());
     const { id } = useParams(':id');
+    const [material, setMaterial] = useState({});
 
     const handleLoadEstoque = async () => {
         const request = await fetch(`http://localhost:8000/estoques/${id}/`);
         const response = await request.json();
-
-        setEstoque(response);
+    
+        if (response.material) {
+            setEstoque(response);
+            const requestMaterial = await fetch(`http://localhost:8000/materiais/${response.material}/`);
+            const responseMaterial = await requestMaterial.json();
+            setMaterial(responseMaterial);
+        } else {
+            console.error("Propriedade 'material' não encontrada na resposta da primeira requisição.");
+        }
     };
+
+
 
     useEffect(() => {
         handleLoadEstoque();
-    });
+    }, []);
 
     return(
         <>
-            <Title>Mais Informações: {estoque.material}</Title>
+            <Title>Mais Informações: {material.nome}</Title>
         </>
     );
 };
