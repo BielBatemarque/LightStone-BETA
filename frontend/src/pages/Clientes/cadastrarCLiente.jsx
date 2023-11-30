0f28c3e1eed0560d3b89fb052ce1b76a68d6a10f
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-11-30 00:36:22 -0300

    feat: styles

--- frontend/src/pages/Clientes/cadastrarCLiente.jsx
+++ frontend/src/pages/Clientes/cadastrarCLiente.jsx
@@ -5,7 +5,7 @@ import { Cliente } from "../../models/Cliente";
 import { globalContext } from "../../context/context";
 import { FailNotifications, SucssesNotifications } from "../../components/Notifications";
 import { useNavigate } from "react-router-dom";
-import { FundoTitle } from "./styles";
+import { FundoForm, FundoTitle } from "./styles";
 
 export const CadastrarCLientePage = () => {
     const [cliente, setCliente] = useState(new Cliente());
@@ -44,16 +44,18 @@ export const CadastrarCLientePage = () => {
             <FundoTitle>
                 <Title mt={0}>Cadastrar Cliente</Title>
             </FundoTitle>
-            <form>
-                <input type="text" name="nome" onChange={handleChange} placeholder="Nome"/> <br />
-                <input type="text" name="cpf" onChange={handleChange} placeholder="CPF"/> <br />
-                <textarea name="endereco" id="" cols="30" rows="3" onChange={handleChange} placeholder="Endereço"></textarea><br />
-                <input type="date" name="data_nascimento" onChange={handleChange} placeholder="Nascimento"/><br />
-                <input type="email" name="email" onChange={handleChange} placeholder="email"/><br />
+            <FundoForm>
+                <form>
+                    <input type="text" name="nome" onChange={handleChange} placeholder="Nome"/> <br />
+                    <input type="text" name="cpf" onChange={handleChange} placeholder="CPF"/> <br />
+                    <textarea name="endereco" id="" cols="30" rows="3" onChange={handleChange} placeholder="Endereço"></textarea><br />
+                    <input type="date" name="data_nascimento" onChange={handleChange} placeholder="Nascimento"/><br />
+                    <input type="email" name="email" onChange={handleChange} placeholder="email"/><br />
 
-                <Button action={handleCadastraCliente}>Cadastrar</Button>
+                    <Button action={handleCadastraCliente}>Cadastrar</Button>
 
-            </form>
+                </form>
+            </FundoForm>
         </>
     );
 };
