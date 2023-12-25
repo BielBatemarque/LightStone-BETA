60e65f65ca4da10390f9395244e21791c40e509a
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-12-25 10:57:35 -0300

    fix: componente floatLabel

--- frontend/src/components/InputLabel/index.jsx
+++ frontend/src/components/InputLabel/index.jsx
@@ -1,9 +1,10 @@
 import { StyledInput, StyledLabel, StyledDiv } from './styles';
 
-export const InputLabel = () => {
+export const InputLabel = ({ text, onChange, value, name, type}) => {
     return(
         <StyledDiv>
-
+            <StyledInput type={type} placeholder=" " onChange={onChange} value={value} name={name}/>
+            <StyledLabel>{text}</StyledLabel>
         </StyledDiv>
     );
 };
