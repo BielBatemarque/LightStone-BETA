4d8258189a32addf80be393850e21a4146c5f1a6
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-12-27 00:10:26 -0300

    Feat: adicionando material a model

--- backend/orcamentos/models.py
+++ backend/orcamentos/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from clientes.models import Cliente
+from materiais.models import Material
 
 # Create your models here.
 class Orcamento(models.Model):
     valor_total = models.FloatField()
     cliente = models.ForeignKey(Cliente, on_delete=models.DO_NOTHING)
+    materiais = models.ManyToManyField(Material)
     
