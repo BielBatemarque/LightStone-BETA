ec92a41e130c8cd628f31a209addd65cc4e05beb
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-12-27 00:11:23 -0300

    Feat: app de peças, model de peças

---
+++ backend/pecas/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Peca
+# Register your models here.
+
+
+admin.site.register(Peca)
---
+++ backend/pecas/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from materiais.models import Material
+
+# Create your models here.
+
+class Peca(models.Model):
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=100)
+    dimensoes = models.TextField()
