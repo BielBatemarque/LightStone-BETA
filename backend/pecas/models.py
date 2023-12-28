8b72957794eca11953087e3b7d32200f5e97af53
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-12-28 00:26:39 -0300

    fix: model peças

--- backend/pecas/models.py
+++ backend/pecas/models.py
@@ -7,3 +7,6 @@ class Peca(models.Model):
     material = models.ForeignKey(Material, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100)
     dimensoes = models.TextField()
+
+    def __str__(self) -> str:
+        return self.nome
