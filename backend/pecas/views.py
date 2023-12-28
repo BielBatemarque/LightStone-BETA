df8fa3791d5834abb4fb63f8c2e1cd28e91a5e46
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-12-28 00:26:28 -0300

    feat: viewsets de peças

--- backend/pecas/views.py
+++ backend/pecas/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Peca
+from .serializers import PecaSerializers
 # Create your views here.
+
+class PecasViewSets(viewsets.ModelViewSet):
+    queryset = Peca.objects.all()
+    serializer_class = PecaSerializers
