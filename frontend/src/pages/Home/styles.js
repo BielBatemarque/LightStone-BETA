511b56b43c5bc96c1a4b787b99e1d26e3fe7b9bb
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-12-30 00:03:03 -0300

    feat: componente de FundoDashBoard

---
+++ frontend/src/pages/Home/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const FundoDashBoard = styled.div`
+    background-color: lightgray;
+    width: 95%;
+    margin: auto;
+    border-radius: 5px;
+    height: 95%;
+`;
