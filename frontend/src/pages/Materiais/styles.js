81a13fd1adb73604e01dfeb0ea21990bf6013d49
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-12-30 00:04:07 -0300

    feat: componentes de styledSelect e StyledOption

---
+++ frontend/src/pages/Materiais/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const StyledSelect = styled.select`
+    width: 12rem;
+    height: 2rem;
+    border: none;
+    border-radius: 5px;
+    padding: 2px;
+`;
+
+export const StyledOptions = styled.option`
+    padding: 5px;
+    height: 2rem;
+    margin: 5px;
+`;
