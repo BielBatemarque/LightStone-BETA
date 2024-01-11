96a42e795b3e4b0adbca3ee949a310737ac2eb18
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-01-10 21:58:38 -0300

    fix: div de card

--- frontend/src/components/Card/styles.js
+++ frontend/src/components/Card/styles.js
@@ -1,3 +1,7 @@
 import styled from "styled-components";
 
-export const CardDiv = styled.div``;
+export const CardDiv = styled.div`
+    background-color: red;
+    height: 20rem;
+    border-radius: 10px;
+`;
