adcf8d70ee6c39c8d2af1cb7013d2b20fbf5aa0b
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-01-15 18:24:04 -0300

    fix: estilização da modal

--- frontend/src/components/Modal/styles.js
+++ frontend/src/components/Modal/styles.js
@@ -2,4 +2,11 @@ import styled from "styled-components";
 
 export const ModalDiv = styled.div`
     background-color: red;
+    width: 30%;
+    height: 35%;
+    top: 50%;
+    left: 50%;
+    position: absolute;
+    transform: translate(-50%, -50%);
+    display: ${(isOpen) => (isOpen ? 'block' : 'none')};
 `;
