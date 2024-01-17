1af283d8992b6071178687a9840907acc26e67d6
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-01-17 20:00:50 -0300

    Feat: trazendo estado para o componente App.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,18 +6,23 @@ import { FlexCointainer } from './components/FlexContainer';
 import { SideBar } from './components/SideBar';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import { ScreenContext, ScrenState } from './context/screenContext';
 
 function App() {
   const { state } = useContext(globalContext);
+  const screenState = useContext(ScreenContext);
 
+  console.log(screenState);
   return (
     <div className="App">
       <BrowserRouter>
-        <ToastContainer />
-        { state.logado ? <FlexCointainer>
-            <SideBar />
-            <Content />
-        </FlexCointainer> : <Content /> }
+        <ScrenState>
+          <ToastContainer />
+          { state.logado ? <FlexCointainer>
+              <SideBar />
+              <Content />
+          </FlexCointainer> : <Content /> }
+        </ScrenState>
       </BrowserRouter>
     </div>
   );
