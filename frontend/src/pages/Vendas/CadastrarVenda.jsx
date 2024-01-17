64307fb263deddc60c75e6774e5b57e547834621
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-01-17 20:00:33 -0300

    Fix: componente de chamar modal

--- frontend/src/pages/Vendas/CadastrarVenda.jsx
+++ frontend/src/pages/Vendas/CadastrarVenda.jsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
 import { Modal } from "../../components/Modal";
 import { Title } from "../../components/Title";
+import { FundoForm, FundoTitle } from "../Clientes/styles";
 
 export const CadastrarVenda = () => {
     const [isOpen, setIsOpen] = useState(true);
     console.log(isOpen);
     return(
         <>
-            <Title>Nova Venda</Title>
-            <Modal  isOpen={isOpen} text="Deseja cadastrar um novo orçamento?"/>
+            <FundoTitle>
+                <Title mt={0}>Nova Venda</Title>
+            </FundoTitle>
+            <FundoForm>
+                <Modal  isOpen={isOpen} text="Deseja cadastrar um novo orçamento?"/>
 
-            <button onClick={() => setIsOpen(s => !s)}>abrir e fechar</button>
+                <button onClick={() => setIsOpen(s => !s)}>abrir e fechar</button>
+            </FundoForm>
         </>
     );
 };
