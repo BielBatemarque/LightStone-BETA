cc785670aaf877ca83d30ea37e66830ab11183a7
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-01-18 00:19:19 -0300

    fix: estilização da pagina

--- frontend/src/pages/Vendas/MaisInformacoesVendas.jsx
+++ frontend/src/pages/Vendas/MaisInformacoesVendas.jsx
@@ -1,11 +1,17 @@
 import { useParams } from "react-router-dom";
 import { Title } from "../../components/Title";
+import { FundoForm, FundoTitle } from "../Clientes/styles";
 
 export const MaisInformacoesVenda = () => {
     const { id } = useParams(':id');
     return(
         <>
-            <Title>Venda: {id}</Title>
+            <FundoTitle>
+                <Title mt={0}>Venda: {id}</Title>
+            </FundoTitle>
+            <FundoForm>
+                
+            </FundoForm>
         </>
     );
 }
