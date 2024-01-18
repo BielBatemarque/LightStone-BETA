0a3d6feb2bbd5469711c4827712b42aa81683b96
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-01-18 00:19:08 -0300

    feat: função que altera estado

--- frontend/src/pages/Vendas/CadastrarVenda.jsx
+++ frontend/src/pages/Vendas/CadastrarVenda.jsx
@@ -1,21 +1,30 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Modal } from "../../components/Modal";
 import { Title } from "../../components/Title";
 import { FundoForm, FundoTitle } from "../Clientes/styles";
+import { ScreenContext } from "../../context/screenContext";
+
 
 export const CadastrarVenda = () => {
+    const { screenState, dispatch } = useContext(ScreenContext);
     const [isOpen, setIsOpen] = useState(true);
+  
+    const toggleModal = () => {
+      dispatch({ type: isOpen ? 'Closed' : 'Opening' });
+      setIsOpen((prevState) => !prevState);
+    };
+  
     console.log(isOpen);
-    return(
-        <>
-            <FundoTitle>
-                <Title mt={0}>Nova Venda</Title>
-            </FundoTitle>
-            <FundoForm>
-                <Modal  isOpen={isOpen} text="Deseja cadastrar um novo orçamento?"/>
-
-                <button onClick={() => setIsOpen(s => !s)}>abrir e fechar</button>
-            </FundoForm>
-        </>
+  
+    return (
+      <>
+        <FundoTitle>
+          <Title mt={0}>Nova Venda</Title>
+        </FundoTitle>
+        <FundoForm modalOpen={screenState.modalOpen}>
+          <Modal isOpen={isOpen} text="Deseja cadastrar um novo orçamento?" />
+          <button onClick={toggleModal}>Abrir e fechar</button>
+        </FundoForm>
+      </>
     );
-};
+  };
