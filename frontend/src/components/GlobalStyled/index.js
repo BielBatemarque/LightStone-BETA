4a29dc037092e9a9885d2ca34a4b9e9eb6b2014b
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-01-18 00:18:40 -0300

    feat: criação de global style

---
+++ frontend/src/components/GlobalStyled/index.js
@@ -0,0 +1,7 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyled = createGlobalStyle`
+ body.darken-background {
+    background-color: rgba(0, 0, 0, 0.5); /* Ajuste a opacidade conforme necessário */
+  }
+`;
