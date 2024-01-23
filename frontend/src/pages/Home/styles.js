7984b2ad821c734bb754a2fa7d220d7111299467
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-01-22 21:46:25 -0300

    Fix: alterando fundo dashboard

--- frontend/src/pages/Home/styles.js
+++ frontend/src/pages/Home/styles.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 export const FundoDashBoard = styled.div`
     background-color: lightgray;
-    width: 95%;
+    width: 92%;
     margin: auto;
-    border-radius: 5px;
-    height: 95%;
+    border-radius: 0px 0px 5px 5px;
+    height: 90%;
 `;
