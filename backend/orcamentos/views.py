e5e2808c4784918e6559dc64ce3988f7f63ee797
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-01-23 21:00:28 -0300

    Feat: viewsets de orçamentos

--- backend/orcamentos/views.py
+++ backend/orcamentos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Orcamento
+from .serializers import OrcamentoSerializer
 # Create your views here.
+
+class OrcamentoViewSets(viewsets.ModelViewSet):
+    queryset = Orcamento.objects.all()
+    serializer_class = OrcamentoSerializer
