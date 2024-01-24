9eae66abfab6d3fea7740321a2360455bdff21c4
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-01-23 21:01:00 -0300

    Fix: models de Vendas

--- backend/vendas/models.py
+++ backend/vendas/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from clientes.models import Cliente
+from orcamentos.models import Orcamento
 # Create your models here.
 
 class Venda(models.Model):
     valor_total = models.FloatField()
     cliente = models.ForeignKey(Cliente, on_delete=models.DO_NOTHING)
+    orcamento = models.ForeignKey(Orcamento, on_delete=models.DO_NOTHING, null=True)
     
     def __str__(self) -> str:
         return self.cliente.nome
