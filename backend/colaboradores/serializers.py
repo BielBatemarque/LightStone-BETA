d5d4fefba249b0105acf7bfcb99d30bd9da05a16
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-01-25 21:45:11 -0300

    feat: validação de cpf de colaborador

--- backend/colaboradores/serializers.py
+++ backend/colaboradores/serializers.py
@@ -7,3 +7,10 @@ class ColaboradorSerializer(serializers.ModelSerializer):
         model = Colaborador
         fields = ['id', 'nome', 'nascimento', 'telefone', 'cpf', 'email', 'cargo']
 
+    def validate_cpf(self, value):
+        # Valida se ja existe algum colaborador com o cpf
+        cpf_existente = Colaborador.objects.filter(cpf=value).exists()
+
+        if cpf_existente:
+            raise serializers.ValidationError("CPF já cadastrado")
+        return value
