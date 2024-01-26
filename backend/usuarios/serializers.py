8cb30d8166e2c842335372ff36b8f63bbea170f9
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-01-26 00:27:10 -0300

    Fix: Validação para username de usuário

--- backend/usuarios/serializers.py
+++ backend/usuarios/serializers.py
@@ -8,5 +8,10 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'url', 'username', 'email', 'is_staff', 'password']
 
-class LogoutSerializer(serializers.Serializer):
-    pass
+    def validade_username(self, value):
+        # Valida se username já esta em uso
+        username_existente = User.objects.filter(username=value).exists()
+
+        if username_existente:
+            raise serializers.ValidationError("Username já cadastrado")
+        return value
