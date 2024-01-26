ad1bc1832ba132c744c38053f612d15a8819951d
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-01-25 21:44:32 -0300

    Feat: valida se o cpj ja existe

--- backend/clientes/serializers.py
+++ backend/clientes/serializers.py
@@ -1,4 +1,3 @@
-from django.urls import path, include
 from .models import Cliente
 from rest_framework import routers, serializers, viewsets
 
@@ -12,3 +11,13 @@ class ClienteSerializer(serializers.ModelSerializer):
         if not "@" in (value) and not ".com" in value:
             raise serializers.ValidationError("O email não está em um formato válido.")
         return value
+    
+    def validate_cpf(self, value):
+        # Verifica se o CPF de cadastro já existe no banco
+        cpf_existente = Cliente.objects.filter(cpf=value).exclude(id=self.instance.id if self.instance else None).first()
+
+        if cpf_existente:
+            raise serializers.ValidationError("Este CPF já está em uso com outro Cliente")
+
+        return value
+    
