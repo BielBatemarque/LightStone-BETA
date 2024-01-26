38b9908e47fa9a17905a5b003a5c2e959d55bddf
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-01-26 00:25:55 -0300

    Fix: Colocado nome como chave unica

--- backend/materiais/models.py
+++ backend/materiais/models.py
@@ -3,7 +3,7 @@ from fornecedores.models import Fornecedor
 
 # Create your models here.
 class Material(models.Model):
-    nome = models.CharField(max_length=50)
+    nome = models.CharField(max_length=50, unique=True)
     cor_base = models.CharField(max_length=15)
     fornecedor = models.ManyToManyField(Fornecedor)
     
