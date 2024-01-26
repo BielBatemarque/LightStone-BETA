fd3103b0ed894d971b11243829d409ea6c9ff179
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-01-25 21:45:46 -0300

    Feat: validação para cnpj de fornecedor

--- backend/fornecedores/serializers.py
+++ backend/fornecedores/serializers.py
@@ -6,3 +6,11 @@ class FornecedorSerializers(serializers.ModelSerializer):
     class Meta:
         model = Fornecedor
         fields = ['id', 'nome_empresa', 'cnpj', 'endereco']
+
+        def validade_cnpj(self, value):
+            #Verifica se o cnpj ja existe
+            cnpj_existe = Fornecedor.objects.filter(cnpj=value).exists()
+
+            if cnpj_existe:
+                raise serializers.ValidationError("CNPJ ja cadastrado")
+            return value
