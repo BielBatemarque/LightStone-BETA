6a269d57edde5ddccbe54e911747ad628ec4384a
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-01-25 21:45:30 -0300

    Fix: cnpj de forncedor chave unica

--- backend/fornecedores/models.py
+++ backend/fornecedores/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Fornecedor(models.Model):
     nome_empresa = models.CharField(max_length=50)
-    cnpj = models.IntegerField()
+    cnpj = models.IntegerField(unique=True)
     endereco = models.TextField(max_length=150)
 
     def __str__(self):
