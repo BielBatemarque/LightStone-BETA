690a7d7b242d512e02b57a2b32facfaa0ac0c715
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-01-26 00:26:11 -0300

    Feat: validação para nome do material

--- backend/materiais/serializers.py
+++ backend/materiais/serializers.py
@@ -5,3 +5,11 @@ class MaterialSerializers(serializers.ModelSerializer):
     class Meta:
         model = Material
         fields = ['id', 'nome', 'cor_base', 'fornecedor']
+
+        def validate_name(self, value):
+            # Valida se o material com o mesmo nome já está cadastrado
+            nome_existente = Material.objects.filter(nome=value)
+
+            if nome_existente:
+                raise serializers.ValidationError("Material já cadastrado")
+            return value
