5fc4f237d01312e1791e66fd204cafab2b27b274
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-01-31 20:36:13 -0300

    Feat: componentes de filtro

--- frontend/src/components/Filtro/index.jsx
+++ frontend/src/components/Filtro/index.jsx
@@ -1,7 +1,11 @@
-export const Filtro = ({ lista }) => {
-    return(
-        <>
+import { StyledBtnFilter, StyledInputFilter } from './/styles';
+import { CiFilter } from "react-icons/ci";
+import { FlexDiv } from '../../pages/Clientes/styles';
 
-        </>
+export const Filtro = ({ lista, text }) => {
+    return(
+        <FlexDiv>
+            <StyledInputFilter placeholder={text} /> <StyledBtnFilter><CiFilter /></StyledBtnFilter>
+        </FlexDiv>
     );
 };
