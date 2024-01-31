b7c7ac76d5f1fdfd83f5ec31a4efba22c7df6497
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-01-31 20:36:43 -0300

    feat: adicionando filtro

--- frontend/src/pages/Clientes/index.jsx
+++ frontend/src/pages/Clientes/index.jsx
@@ -5,6 +5,7 @@ import { FlexCointainer } from "../../components/FlexContainer";
 import { Title } from "../../components/Title";
 import { Button } from "../../components/Button";
 import { useNavigate } from 'react-router-dom';
+import { Filtro } from "../../components/Filtro";
 
 export const ClientesPage = () => {
     const[clientes, setClientes] = useState([]);
@@ -25,6 +26,7 @@ export const ClientesPage = () => {
         <>
             <FlexCointainer pontas='true' size={'92%'}>
                 <Title>Clientes</Title>
+                <Filtro />
                 <Button action={() => navegate('/Clientes/cadastrarCliente/')}>Novo Cliente</Button>
            </FlexCointainer>
            <Listing>
