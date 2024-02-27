20e70e648cf685bfa4f4bf48e9182c4ed502aea0
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-02-26 22:36:07 -0300

    fix: estilo do componente

--- frontend/src/components/Filtro/styles.js
+++ frontend/src/components/Filtro/styles.js
@@ -1,5 +1,14 @@
 import styled from "styled-components";
 
-export const StyledInputFilter = styled.input``;
+export const StyledInputFilter = styled.input`
+    height: 1.2rem;
+`;
 
-export const StyledBtnFilter = styled.button``;
+export const StyledBtnFilter = styled.button`
+    border-radius: 50%;
+    height: 2rem;
+    border: solid 1px;
+    background-color:  rgb(42, 42, 42);;
+    color: white;
+    cursor: pointer;
+`;
