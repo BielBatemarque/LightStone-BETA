ee7e26ea4dd9fee121d5368d6199fbe0cfd4fc0c
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-02-28 22:48:58 -0300

    feat: novo componente de modal

--- frontend/src/components/Modal/index.jsx
+++ frontend/src/components/Modal/index.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
 import { ModalDiv } from "./styles";
+import Modal from 'react-modal';
 
-export const Modal = ({text, isOpen}) => {
+Modal.setAppElement('#root');
+export const ModalComponent = ({text, isOpen}) => {
+    const [modasIsOpen, setModalIsOpen] = useState(false);
+    
+    const openModal= () => {
+        setModalIsOpen(true);
+    }
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    }
+
     return(
-        <ModalDiv isOpen={isOpen}>
-            {text}
-        </ModalDiv>
+        <Modal
+            isOpen={modasIsOpen}
+            onRequestClose={closeModal}
+            contentLabel="Example Modal"
+            overlayClassName='modal-overlay'
+            className='modal-content'
+        >
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis incidunt itaque officiis numquam dolorem! Nobis fugit repellat illum rem consectetur quae, harum omnis similique quaerat! Fuga excepturi est facere cupiditate!</p>
+
+            <button onClick={closeModal}>fecha modal</button>
+        </Modal>
     );
 }
