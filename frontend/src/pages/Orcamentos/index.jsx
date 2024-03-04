83ae8ef0b616600b25b8109ed3bd83fe35f72ed0
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-03-04 20:40:35 -0300

    colocando title na pagina

--- frontend/src/pages/Orcamentos/index.jsx
+++ frontend/src/pages/Orcamentos/index.jsx
@@ -1,7 +1,16 @@
+import { FlexCointainer } from "../../components/FlexContainer";
+import { Listing } from "../../components/Listing";
+import { Title } from "../../components/Title";
+
 export const OrcamentosPage = () => {
     return(
         <>
-            
+            <FlexCointainer>
+                <Title>Orçamentos</Title>
+            </FlexCointainer>
+            <Listing>
+
+            </Listing>
         </>
     );
 }
