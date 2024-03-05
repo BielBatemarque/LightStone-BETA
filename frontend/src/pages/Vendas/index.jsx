08c6fe0093561f061799cb03753e5f3744457abe
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-03-05 17:28:26 -0300

    feat: roteamento para orçamentos

--- frontend/src/pages/Vendas/index.jsx
+++ frontend/src/pages/Vendas/index.jsx
@@ -5,6 +5,7 @@ import { Title } from "../../components/Title";
 import { Item } from '../../components/ItemListagem';
 import { Button } from '../../components/Button';
 import { useNavigate } from 'react-router-dom';
+import { ContainerBtns } from "../Estoques/styles";
 
 export const VendasPage = () => {
     const [vendas, setVendas] = useState([]);
@@ -25,7 +26,10 @@ export const VendasPage = () => {
         <>
             <FlexCointainer pontas="true" size={'93%'}>
                 <Title>Vendas</Title>
-                <Button action={() => navigate('/Vendas/CadastrarVenda/')}>Nova Venda</Button>
+                <ContainerBtns>
+                    <Button action={() => navigate('/Orcamentos/')} color={'gray'}>Novo Orçamento</Button>
+                    <Button action={() => navigate('/Vendas/CadastrarVenda/')}>Nova Venda</Button>
+                </ContainerBtns>
             </FlexCointainer>
             <Listing>
                 {vendas.map((venda, index) => (
