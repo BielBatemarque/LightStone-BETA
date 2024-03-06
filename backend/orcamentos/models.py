e0f669bd9755ee651d684f36bd0382c5f4faf351
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-03-06 16:14:55 -0300

    fix: remove a diciona peça do orcamento

--- backend/orcamentos/models.py
+++ backend/orcamentos/models.py
@@ -7,6 +7,12 @@ class Orcamento(models.Model):
     valor_total = models.FloatField()
     cliente = models.ForeignKey(Cliente, on_delete=models.DO_NOTHING)
     pecas = models.ManyToManyField(Peca)
+
+    def adicionar_peca(self, peca):
+        self.pecas.add(peca)
+
+    def remove_peca(self, peca):
+        self.pecas.remove(peca)
     
     def __str__(self) -> str:
         return self.cliente.nome
