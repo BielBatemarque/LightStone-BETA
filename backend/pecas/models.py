083efc55278b283beb5a6f867b4853d015b32407
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-03-05 21:55:48 -0300

    fix: model de peças

--- backend/pecas/models.py
+++ backend/pecas/models.py
@@ -6,7 +6,7 @@ from materiais.models import Material
 class Peca(models.Model):
     material = models.ForeignKey(Material, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100)
-    descrição = models.TextField(null=True)
+    descrição = models.TextField(max_length=100, null=True)
     quantidade_metros = models.FloatField(null=True)
 
     def __str__(self) -> str:
