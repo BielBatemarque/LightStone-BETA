cfa536108b8a72977e2b258f75e6d1a09f9ccddc
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-03-05 21:54:56 -0300

    fix: model de peças

--- backend/orcamentos/models.py
+++ backend/orcamentos/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from clientes.models import Cliente
-from materiais.models import Material
+from pecas.models import Peca
 
 # Create your models here.
 class Orcamento(models.Model):
     valor_total = models.FloatField()
     cliente = models.ForeignKey(Cliente, on_delete=models.DO_NOTHING)
-    material = models.ManyToManyField(Material)
+    pecas = models.ManyToManyField(Peca)
     
     def __str__(self) -> str:
         return self.cliente.nome
