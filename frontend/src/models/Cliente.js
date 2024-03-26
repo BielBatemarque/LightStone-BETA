1d5b8377e854a602dcf149a774d83569da247ab0
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-03-26 19:30:19 -0300

    feat: Atributos de endereço na model frontend de endereço

--- frontend/src/models/Cliente.js
+++ frontend/src/models/Cliente.js
@@ -1,9 +1,14 @@
 export class Cliente{
-    constructor(nome, cpf, endereco, data_nascimento, email){
+    constructor(nome, cpf, data_nascimento, email, cep, numero, cidade, uf, logradouro, bairro){
         this.nome = nome;
         this.cpf = cpf;
-        this.endereco = endereco;
         this.data_nascimento = data_nascimento;
         this.email = email;
+        this.cep = cep;
+        this.numero = numero;
+        this.cidade = cidade;
+        this.uf = uf;
+        this.logradouro = logradouro;
+        this.bairro = bairro;
     };
 };
