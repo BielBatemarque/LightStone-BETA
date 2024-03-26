783b334472ec2398039455ede78367e60e8a20e3
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-03-26 19:29:45 -0300

    Fix: atributos de endereço model de clientes

--- backend/clientes/models.py
+++ backend/clientes/models.py
@@ -5,7 +5,12 @@ from django.db import models
 class Cliente(models.Model):
     nome = models.CharField(max_length=50)
     cpf = models.IntegerField(unique=True)
-    endereco = models.TextField(max_length=150)
+    cep = models.IntegerField(null=True)
+    numero = models.IntegerField(null=True)
+    cidade = models.CharField(max_length=50, null=True)
+    uf = models.CharField(max_length=5, null=True)
+    logradouro = models.CharField(max_length=100, null=True)
+    bairro = models.CharField(max_length=100, null=True)
     data_nascimento = models.DateField()
     email = models.EmailField(max_length=150)
 
