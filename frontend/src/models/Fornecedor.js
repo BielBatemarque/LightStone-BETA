40d73b5367da75ad721ebe2b6d04c029e63aceeb
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-03-29 14:37:21 -0300

    Fix: alterando os campos de endereço da classe de Fornecedor no front-end

--- frontend/src/models/Fornecedor.js
+++ frontend/src/models/Fornecedor.js
@@ -1,7 +1,12 @@
 export class Fornecedor{
-    constructor(nomeEmpresa, cnpj, endereco){
+    constructor(nomeEmpresa, cnpj, cep, numero, cidade, uf, logradouro, bairro){
         this.nome_empresa = nomeEmpresa;
         this.cnpj = cnpj;
-        this.endereco = endereco;
+        this.cep = cep;
+        this.numero = numero;
+        this.cidade = cidade;
+        this.uf = uf;
+        this.logradouro = logradouro;
+        this.bairro = bairro;
     };
 };
