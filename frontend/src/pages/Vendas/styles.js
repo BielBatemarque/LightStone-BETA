2036b3774e1b8c548333185328d215cc55a8961a
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-04-11 21:17:01 -0300

    feat: estilização para vendas

---
+++ frontend/src/pages/Vendas/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Linha = styled.div`
+    text-align: center;
+    display: flex;
+    width: 70%;
+    justify-content: space-evenly;
+    align-items: center;
+`;
