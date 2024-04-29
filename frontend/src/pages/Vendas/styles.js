ab37ee112056e45c62f14d3c09a6c1f9eeffb2db
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-04-29 18:30:16 -0300

    tirando vendas

--- frontend/src/pages/Vendas/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Linha = styled.div`
-    text-align: center;
-    display: flex;
-    width: 70%;
-    justify-content: space-evenly;
-    align-items: center;
-`;
