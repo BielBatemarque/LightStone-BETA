be86c5b0078d5f95b644bc29da978081f05f9081
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-07-07 14:33:49 -0300

    Feat: model de cargos

--- backend/colaboradores/models.py
+++ backend/colaboradores/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Cargos(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.nome
+
 class Colaborador(models.Model):
     nome = models.CharField(max_length=80)
     nascimento = models.DateField()
     telefone = models.CharField(max_length=15)
     cpf = models.TextField(max_length=11, unique=True)
     email = models.CharField(max_length=100)
-    cargo = models.CharField(max_length=100, null=True)
+    cargo = models.ForeignKey(Cargos, on_delete=models.DO_NOTHING, null=True)
 
     def __str__(self) -> str:
         return self.nome
