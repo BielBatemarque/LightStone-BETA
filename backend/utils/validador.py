f5d9f146f2a433f5a3d2f660e3acd34f0323f8c6
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-08-03 16:26:22 -0300

    Feat: criando validador

--- backend/utils/validador.py
+++ backend/utils/validador.py
@@ -1,10 +1,10 @@
+from utils.formatador import Formatador
+
 class Validador:
 
-    def valida_cpf(cpf):
-        pass
-
-    def valida_cnpj(cnpj):
-        pass
+    def valida_cpf_cnpj(documento: str) -> bool:
+        if len(documento) != 12 or len(documento) != 14:
+            return False
 
     def valida_cep(cep):
         pass
