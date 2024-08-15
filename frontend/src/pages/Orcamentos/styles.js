ec0d3df21504fa5bc629f7c9cb8f9a312d039d46
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-08-14 23:41:09 -0300

    feat: componente de form de peças

---
+++ frontend/src/pages/Orcamentos/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+
+export const FlexDivFooter = styled.div`
+    display: flex;
+    text-align: center;
+    width: 100%;
+    justify-content: space-evenly;
+    margin-bottom: 0px;
+`;
+
+export const FormLinePecas = styled.div`
+    display: flex;
+    background-color: red;
+`;
