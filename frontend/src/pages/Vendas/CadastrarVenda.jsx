38efd05978b99965ce27c5d64d220b031146c615
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-10-05 20:54:50 -0300

    feat: finalizando vendas

--- frontend/src/pages/Vendas/CadastrarVenda.jsx
+++ frontend/src/pages/Vendas/CadastrarVenda.jsx
@@ -1,80 +1,114 @@
 import { useContext, useState } from "react";
-import { ModalComponent } from "../../components/Modal";
 import { Title } from "../../components/Title";
-import { FlexDiv, FlexRow, FundoForm, FundoTitle, StyledForm } from "../Clientes/styles";
-import { ScreenContext } from "../../context/screenContext";
+import { FlexDiv, FundoForm, FundoTitle, StyledForm } from "../Clientes/styles";
 import { FloatLabel } from "../../components/FloatLabel";
 import { Button } from "../../components/Button";
 import { Linha } from './styles';
-import { AtentionNotification, FailNotifications, SucssesNotifications } from "../../components/Notifications";
-
+import { AtentionNotification, SucssesNotifications } from "../../components/Notifications";
+import { useAuth } from "../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
 export const CadastrarVenda = () => {
-    const [isOpen, setIsOpen] = useState(true);
     const [orcamentosCliente, setOrcamentosClientes] = useState([]);
     const [nomeCliente, setNomeCliente] = useState('');
-    
+    const [orcamentoSelecionado, setOrcamentoSelecionado] = useState(null);
+    const {state} = useAuth();
+    const navigate = useNavigate();
 
     const handleCarregarOrcamentosCliente = async (event) => {
-      event.preventDefault();
+        event.preventDefault();
 
-      if (!nomeCliente){
-        AtentionNotification("Digite o nome do cliente");
-        return;
-      }
+        if (!nomeCliente) {
+            AtentionNotification("Digite o nome do cliente");
+            return;
+        }
 
-      const request = await fetch(`http://localhost:8000/orcamentos/retorna_orcamentos_cliente/?cliente=${nomeCliente}`);
-      const response = await request.json();
+        const request = await fetch(`http://localhost:8000/orcamentos/retorna_orcamentos_cliente/?cliente=${nomeCliente}`);
+        const response = await request.json();
 
-      setOrcamentosClientes(response);
+        setOrcamentosClientes(response);
 
-      if (response.length > 0){
-        SucssesNotifications("Orçamentos Encontrados");
+        if (response.length > 0) {
+            SucssesNotifications("Orçamentos Encontrados");
+        }
+    };
+
+    const handleFinalizarVenda = async () => {
+      const request = await fetch('http://localhost:8000/vendas/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+           'Authorization' : `Token ${state.token}`
+        },
+        body: JSON.stringify({
+          valor_total: orcamentoSelecionado.valor_total,
+          cliente: orcamentoSelecionado.cliente,
+          orcamento: orcamentoSelecionado.id,
+        }),
+      });
+      
+      if (request.ok){
+        SucssesNotifications("Venda finalizada com sucesso.");
+        navigate('/Vendas/');
       }
     }
 
-    console.log(nomeCliente);
-    console.log(orcamentosCliente);
-  
-    return (
-      <>
-        <FundoTitle>
-          <Title mt={0}>Nova Venda</Title>
-        </FundoTitle>
-        <FundoForm>
-            <StyledForm onSubmit={handleCarregarOrcamentosCliente}>
-              <Linha>
-                <FloatLabel text="Pesquisar por cliente" onChange={(e) => setNomeCliente(e.target.value)}/>
-                <Button color={'gray'}>Pesquisar</Button>
-              </Linha>
-            </StyledForm>
 
-            {
-              orcamentosCliente.length > 0 ?
-                <table>
-                  <thead>
-                    <tr>
-                      <td>Selecionar</td>
-                      <td>Quantidade de peças</td>
-                      <td>Valor</td>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {orcamentosCliente.map((orcamento, index) => (
-                      <tr key={index}>
-                        <td><input type="checkbox"/></td>
-                        <td>{orcamento.pecas.length}</td>
-                        <td>{orcamento.valor_total}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-                : null
-            }
-            <FlexDiv>
-              <Button>Finalizar Venda</Button>
-            </FlexDiv>
-        </FundoForm>
-      </>
+    // Função para lidar com a seleção do orçamento
+    const handleSelectOrcamento = (orcamento) => {
+        setOrcamentoSelecionado(orcamentoSelecionado?.id === orcamento.id ? null : orcamento); // Seleciona/deseleciona o orçamento
+    };
+
+    console.log("Cliente:", nomeCliente);
+    console.log("Orçamentos:", orcamentosCliente);
+    console.log("Orçamento Selecionado:", orcamentoSelecionado);
+
+    return (
+        <>
+            <FundoTitle>
+                <Title mt={0}>Nova Venda</Title>
+            </FundoTitle>
+            <FundoForm>
+                <StyledForm onSubmit={handleCarregarOrcamentosCliente}>
+                    <Linha>
+                        <FloatLabel text="Pesquisar por cliente" onChange={(e) => setNomeCliente(e.target.value)} />
+                        <Button color={'gray'}>Pesquisar</Button>
+                    </Linha>
+                </StyledForm>
+
+                {orcamentosCliente.length > 0 && (
+                    <FlexDiv>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <td>Selecionar</td>
+                                    <td>Quantidade de peças</td>
+                                    <td>Valor</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {orcamentosCliente.map((orcamento, index) => (
+                                    <tr key={index}>
+                                        <td>
+                                            <input
+                                                type="checkbox"
+                                                checked={orcamentoSelecionado?.id === orcamento.id}
+                                                onChange={() => handleSelectOrcamento(orcamento)}
+                                            />
+                                        </td>
+                                        <td>{orcamento.pecas.length}</td>
+                                        <td>{orcamento.valor_total}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </FlexDiv>
+                )}
+
+                <FlexDiv>
+                    <Button action={() => handleFinalizarVenda()}>Finalizar Venda</Button>
+                </FlexDiv>
+            </FundoForm>
+        </>
     );
-  };
+};
