6deb3a4dcda7215af1af75d3bbbf4b6a676100c4
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-10-07 19:18:40 -0300

    feat: logica para excutar função de filtro

--- frontend/src/components/ListFilter/index.jsx
+++ frontend/src/components/ListFilter/index.jsx
@@ -2,18 +2,13 @@ import { CiFilter } from "react-icons/ci";
 import { Flex } from './styled';
 import { useState } from "react";
  
-export const ListFilter = ({ url, campo_pesquisa }) => {
+export const ListFilter = ({ action }) => {
     const [valor, setValue] = useState("");
-    console.log(valor);
-
-    const handleFilter = async (url_link, ) => {
-        const request = await fetch(url_link)
-    }
-
+    
     return (
         <Flex>
             <input value={valor} onChange={(e) => setValue(e.target.value)}/>
-            <button onClick={() => handleFilter(url)}> <CiFilter/> </button>
+            <button onClick={() => action(valor)}> <CiFilter/> </button>
         </Flex>
     );
 }
