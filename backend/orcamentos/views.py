9a1882f1a7d3840d3391102baca4bb507d505c4d
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-10-08 22:47:41 -0300

    feat: retorna orcamento com pecas, bacckend

--- backend/orcamentos/views.py
+++ backend/orcamentos/views.py
@@ -5,6 +5,7 @@ from .serializers import OrcamentoSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import status
+from pecas.serializers import PecaSerializers
 
 class OrcamentoViewSets(viewsets.ModelViewSet):
     queryset = Orcamento.objects.all()
@@ -22,3 +23,23 @@ class OrcamentoViewSets(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
         
         Response({"datail": "Cliente não encontrado"}, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=["post"])
+    def retorna_orcamento_com_pecas(self, request):
+        orcamento_id = request.data.get("id", None)
+
+        if not orcamento_id:
+            return Response({"datail": "O campo id é obrigatorio"}, status=status.HTTP_400_BAD_REQUEST)
+       
+        try:
+            orcamento = Orcamento.objects.get(id=orcamento_id)
+        except Orcamento.DoesNotExist:
+            return Response({"detail": "Orçamento não encontrado."}, status=status.HTTP_404_NOT_FOUND)
+       
+        orcamento_serializer = self.get_serializer(orcamento)
+        pecas = orcamento.pecas.all()
+        pecas_serialier = PecaSerializers(pecas, many=True)
+
+        data = orcamento_serializer.data
+        data["pecas"] = pecas_serialier.data
+        return Response(data, status=status.HTTP_200_OK)
