76b905436f90f6748538bbaca2f5b5f3d421d3ab
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-11-04 00:10:31 -0300

    Feat: implementando modal de adicionar orçamento

--- frontend/src/components/Modal/CadastrarOrcamentoModal.jsx
+++ frontend/src/components/Modal/CadastrarOrcamentoModal.jsx
@@ -1,15 +1,18 @@
-import { useEffect, useState } from "react";
-import { Title } from "../Title";
+import { useContext, useEffect, useState } from "react";
 import { ButtonContainer, ModalContainer, Overlay } from "./styles";
+import { globalContext } from "../../context/context";
+import { FailNotifications, SucssesNotifications } from "../Notifications";
 
 export const CadastrarOrcamentoModal = ({ isOpen, onClose }) => {
     const [clientes, setClientes] = useState([]);
-    const [formData, setFormData] = useState({
-        nome: '',
-        descricao: '',
-        preco: '',
-        quantidade: '',
+    const [orcamento, setOrcamento] = useState({
+        valor_total: null,
+        cliente: null,
+        pecas: [],
       });
+      const [materiais, setMateriais] = useState([]);
+      const [peca, setPeca] = useState({});
+      const { state } = useContext(globalContext);
 
       const handleCarregaClientes = async () => {
         const request = await fetch('http://localhost:8000/clientes/');
@@ -20,38 +23,110 @@ export const CadastrarOrcamentoModal = ({ isOpen, onClose }) => {
 
       useEffect(() => {
         handleCarregaClientes();
+        handleLoadMateriais();
       }, []);
+
+      const handleLoadMateriais = async () => {
+        const request = await fetch('http://localhost:8000/materiais/');
+        const response = await request.json();
+
+        setMateriais(response);
+      }
 
     const handleChange = (e) => {
         const { name, value } = e.target;
-        setFormData((prevData) => ({ ...prevData, [name]: value }));
-      };
-    
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('Form data:', formData);
-        onClose();
+        setOrcamento((prevData) => ({ ...prevData, [name]: value }));
       };
 
-      if (!isOpen) return null;
+    const handleChangePeca = (e) => {
+        const { name, value } = e.target;
+
+        setPeca({ ...peca, [name]: value });
+    }
+    
+    const handleSubmit = (e) => {
+    e.preventDefault();
+    onClose();
+    };
+
+    if (!isOpen) return null;
+
+    const handleSalvarPeca = async () => {
+        const request = await fetch('http://localhost:8000/pecas/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${state.token}`,
+            },
+            body: JSON.stringify(peca),
+        });
+
+        if (request.ok){
+            SucssesNotifications('Peça adicionada ao Orçamento.');
+
+        } else {
+            FailNotifications('Não foi possivel adicionar a peça ao orçamento.');
+        }
+    } 
 
     return (
         <Overlay>
             <ModalContainer>
-                <Title>Novo Orçamento</Title>
-                <label>
-                    Selecione o cliente:
-                    <select>
-                        {clientes.map((cliente, index) => (
-                            <option key={index} value={cliente.id}>{cliente.nome}</option>
-                        ))}
-                    </select>
-                </label>
+                <h2>Novo Orçamento</h2>
 
-                <ButtonContainer>
-                    <button type="button" onClick={onClose}>Cancelar</button>
-                    <button type="submit">Salvar</button>
-                </ButtonContainer>
+                {/* Formulario do Orcamento */}
+                <form action="">
+
+                    <label>
+                        Selecione o cliente:
+                        <select>
+                            {clientes.map((cliente, index) => (
+                                <option key={index} value={cliente.id}>{cliente.nome}</option>
+                            ))}
+                        </select>
+                    </label>
+
+                    <label htmlFor="">
+                        Valor total do Orçamento:
+                        <input type="number" name="valor_total" />
+                    </label>
+
+                    <ButtonContainer>
+                        <button type="button" onClick={onClose}>Cancelar</button>
+                        <button type="submit">Salvar</button>
+                    </ButtonContainer>
+                </form>
+
+                {/* Formulário para cadastrar as peças do orçamento */}
+                <form onSubmit={handleSalvarPeca}>
+                    <label htmlFor="">
+                        Nome da peça:
+                        <input type="text" name="nome" onChange={handleChangePeca}/>
+                    </label>
+                    <label htmlFor="">
+                        Descrição: 
+                        <input type="text" name="descricao" onChange={handleChangePeca} />
+                    </label>
+
+                    <label htmlFor="">
+                        Quantidade de M²:
+                        <input type="number" name="quantidade_metros" onChange={handleChangePeca}/>
+                    </label>
+
+                    <label htmlFor="">
+                        Material:
+                        <select name="material" onChange={handleChangePeca}>
+                            {materiais.map((material, index) => (
+                                <option key={index} value={material.id}>{material.nome}</option>
+                            ))}
+                        </select>
+                    </label>
+                    
+                    <ButtonContainer>
+
+                        <button type="submit">Incluir Peça</button>
+                    </ButtonContainer>
+                </form>
             </ModalContainer>
         </Overlay>
     );
