09a84593846ddecabc1261d7841776e33e763f52
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-11-03 22:28:11 -0300

    Feat: atualizando pagina de orçamentos

--- frontend/src/pages/Orcamentos/index.jsx
+++ frontend/src/pages/Orcamentos/index.jsx
@@ -5,10 +5,13 @@ import { Listing } from "../../components/Listing";
 import { Title } from "../../components/Title";
 import { Item } from "../../components/ItemListagem";
 import { useNavigate } from "react-router-dom";
+import { ContainerBtns } from "../Estoques/styles";
+import { CadastrarClienteModal } from "../../components/Modal/CadastrarClienteModal";
 
 export const OrcamentosPage = () => {
     const [orcamentos, setOrcamentos] = useState([]);
     const navigate = useNavigate();
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const orcamentosRequest = async () => {
         const request = await fetch(`http://localhost:8000/orcamentos/`);
@@ -23,17 +26,24 @@ export const OrcamentosPage = () => {
 
     console.log(orcamentos);
 
+    const handleOpenModal = () => setIsModalOpen(true);
+    const handleCloseModal = () => setIsModalOpen(false);
+
     return(
         <>
             <FlexCointainer pontas='true' size='93%'>
                 <Title>Orçamentos</Title>
-                <Button action={() => navigate('/Orcamentos/NovoOrcamento/') }>Novo Orçamento</Button>
+                <ContainerBtns>
+                    <Button color={'blue'} action={handleOpenModal}>Novo Cliente</Button>
+                    <Button action={() => navigate('/Orcamentos/NovoOrcamento/') }>Novo Orçamento</Button>
+                </ContainerBtns>
             </FlexCointainer>
             <Listing>
                 {orcamentos.map(((orcamento, index) => (
                     <Item key={index} action={() => navigate(`/Orcamentos/MaisInformacoesOrcamento/${orcamento.id}/`)}>{orcamento.cliente}</Item>
                 )))}
             </Listing>
+            <CadastrarClienteModal isOpen={isModalOpen} onClose={handleCloseModal} />
         </>
     );
 }
