46b93cd6f64961780a3a828631b9e101da4196f7
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-11-22 16:58:56 -0300

    Feat: estilização do componente de filtro

--- frontend/src/components/ListFilter/index.jsx
+++ frontend/src/components/ListFilter/index.jsx
@@ -1,14 +1,27 @@
 import { CiFilter } from "react-icons/ci";
 import { Flex } from './styled';
 import { useState } from "react";
- 
+
 export const ListFilter = ({ action }) => {
     const [valor, setValue] = useState("");
-    
+
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            action(valor);
+        }
+    };
+
     return (
         <Flex>
-            <input value={valor} onChange={(e) => setValue(e.target.value)}/>
-            <button onClick={() => action(valor)}> <CiFilter/> </button>
+            <input 
+                value={valor} 
+                onChange={(e) => setValue(e.target.value)} 
+                placeholder="Digite para filtrar..." 
+                onKeyPress={handleKeyPress}
+            />
+            <button onClick={() => action(valor)}>
+                <CiFilter size={20} />
+            </button>
         </Flex>
     );
-}
+};
--- frontend/src/components/ListFilter/styled.js
+++ frontend/src/components/ListFilter/styled.js
@@ -2,7 +2,40 @@ import styled  from "styled-components";
 
 export const Flex = styled.div`
     display: flex;
-    background-color: red;
-    height: 2.2rem;
-    margin-top: 1.8rem;
+    align-items: center;
+    background-color: #f9f9f9;
+    border: 1px solid #ddd;
+    border-radius: 8px;
+    padding: 0.5rem;
+    width: 94%;
+    margin: 1rem 0;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    margin: 15px auto;
+
+    input {
+        flex: 1;
+        border: none;
+        outline: none;
+        padding: 0.5rem;
+        font-size: 1rem;
+        border-radius: 8px;
+        margin-right: 0.5rem;
+    }
+
+    button {
+        background-color:  rgb(42, 42, 42);
+        color: #fff;
+        border: none;
+        border-radius: 8px;
+        padding: 0.5rem 1rem;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        transition: background-color 0.3s;
+
+        &:hover {
+            background-color: #0056b3;
+        }
+    }
 `;
