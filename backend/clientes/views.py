305a3aa4bafa328b59b6204f40db607d5c539892
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-11-22 17:00:59 -0300

    feat: endpoint de filtrar cliente

--- backend/clientes/views.py
+++ backend/clientes/views.py
@@ -4,14 +4,25 @@ from .serializers import ClienteSerializer
 from .models import Cliente
 from rest_framework.views import APIView, Response
 from rest_framework import status
-from rest_framework.decorators import api_view
 from utils.validador import Validador
+from rest_framework.decorators import action
 # Create your views here.
 
 class ClienteViewSet(viewsets.ModelViewSet):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
 
+    @action(detail=False, methods=["get"], url_name="filtrar_clientes", url_path="filtrar_clientes")
+    def filtrar_clientes(self, request):
+        nome_cliente = request.query_params.get("nome", None)
+
+        if nome_cliente:
+            clientes_filtrados = self.queryset.filter(nome__icontains=nome_cliente)
+            serializer = self.get_serializer(clientes_filtrados, many=True)
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        Response({"datail": "Cliente não encontrado"}, status=status.HTTP_400_BAD_REQUEST)
 
 class ValidaCPF(APIView):
     def post(self, request):
