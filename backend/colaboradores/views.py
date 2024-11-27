ce558cfb6ab5562b2039780120e45672f0bb9d99
Gabriel Batemarque <111823663+BielBatemarque@users.noreply.github.com>
2024-11-27 00:03:41 +0000

    Feat: endpoint de filtrar colaborador

--- backend/colaboradores/views.py
+++ backend/colaboradores/views.py
@@ -18,6 +18,18 @@ class ColaboradorViewsSets(viewsets.ModelViewSet):
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    #Verificar depois o funcionamento
+    @action(detail=False, methods=["get"], url_name= "filtrar_colaborador", url_path="filtrar_colaborador")
+    def filtrar_colaborador(self, request):
+        nome_colaborador = request.query_params.get("nome", None)
+        
+        if nome_colaborador:
+            colaborador_filtrado = self.queryset.filter(nome__icontains=nome_colaborador)
+            serializer = self.get_serializer(colaborador_filtrado)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response({"detail": "Nenhum colaborador com este nome foi encontrado"}, status=status.HTTP_400_BAD_REQUEST)
+
 
 class CargoViewSets(viewsets.ModelViewSet):
     queryset = Cargos.objects.all()
