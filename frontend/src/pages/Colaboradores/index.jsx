66999b69b9ec00d651723a51c408ab89909017d8
Gabriel Batemarque <111823663+BielBatemarque@users.noreply.github.com>
2024-11-27 00:14:32 +0000

    feat: implementando filtro de colaborador + direcionamento e delete

--- frontend/src/pages/Colaboradores/index.jsx
+++ frontend/src/pages/Colaboradores/index.jsx
@@ -1,14 +1,17 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Button } from "../../components/Button";
 import { FlexCointainer } from "../../components/FlexContainer";
 import { Title } from "../../components/Title";
 import { useNavigate } from "react-router-dom";
 import { DataGrid  } from '../../components/Datagrid/styled';
 import { ListFilter } from "../../components/ListFilter";
+import { globalContext } from "../../context/context";
+import { FailNotifications, SucssesNotifications } from "../../components/Notifications";
 
 export const ColaboradorPages = () => {
     const [colabs, setColabs] = useState([]);
     const navigate = useNavigate();
+    const { state } = useContext(globalContext);
 
     const handleLoadingColabs = async () => {
         const request = await fetch('http://localhost:8000/colaboradores/');
@@ -22,6 +25,31 @@ export const ColaboradorPages = () => {
     }, []);
 
     console.log(colabs);
+
+    //Verificar depois o funcionamento
+    const handleFiltrarColaborador = async (nomeColaborador) => {
+        const request = await fetch(`http://localhost:8000/colaboradores/filtrar_colaborador/?nome=${nomeColaborador}`);
+        const responseFiltrado = await request.json();
+
+        setColabs(responseFiltrado);
+    }
+
+    //Verificar depois o funcionamento
+    const handleDeleteColaborador = async (colaboradorId) => {
+        const request = await fetch(`http://localhost:8000/colaboradores/${colaboradorId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${state.token}`,
+            }
+        });
+
+        if (request.ok){
+            SucssesNotifications('Sucesso ao deletar Colaborador');
+        } else {
+            FailNotifications('Erro ao deletar colaborador.');
+        }
+    }
 
     return(
         <>
@@ -49,7 +77,7 @@ export const ColaboradorPages = () => {
                             <td>{colab.email}</td>
                             <td>{colab.cargo_nome}</td>
                             <td className="actions">
-                                <button className="edit">Editar</button>
+                                <button className="edit" onClick={() => navigate(`/colaboradores/${colab.id}`)}>Editar</button>
                                 <button className="delete">Excluir</button>
                             </td>
                         </tr>
